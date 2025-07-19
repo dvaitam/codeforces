d33873174dede58b912020137d74f712f7d5dd5b
Anil <anil.xcode@gmail.com>
2025-07-19 13:25:22 +0000

    cpp solution for 1997

--- 1000-1999/1900-1999/1990-1999/1997/solA.cpp
+++ 1000-1999/1900-1999/1990-1999/1997/solA.cpp
@@ -1,55 +1,19 @@
 #include<bits/stdc++.h>
-#define int long long
 using namespace std;
-
-int helper(string s)
-{
-    int ans=2;
-    for(int i=1;i<s.size();i++)
-    {
-        if(s[i]==s[i-1])
-        {
-            ans++;
-        }
-        else
-        {
-            ans+=2;
-        }
-    }
-    return ans;
-}
-
-string solve()
-{
-    string s;
-    cin>>s;
-    string ans="";
-    int curr=0;
-    for(int i=0;i<=s.size();i++)
-    {
-        for(char ch='a';ch<='z';ch++)
-        {
-            string modified=s.substr(0,i)+ch+s.substr(i,s.size()-i);
-            int time=helper(modified);
-            if(time>curr)
-            {
-                ans=modified;
-                curr=time;
-            }
-        }
-    }
-    return ans;
-}
-
-int32_t main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        cout<<solve()<<"\n";
-    }
-    return 0;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		string s;
+		cin>>s;
+		int i=1;
+		while(s[i-1]!=s[i]&&i<s.length()){
+			i++;
+		}
+		if(s[i-1]=='a')
+		s.insert(i,1,'b');
+		else
+		s.insert(i,1,'a');
+		cout<<s<<endl;
+	}
 }
