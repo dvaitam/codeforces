803db17598887dd4535bc7c92790104717dd3c04
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-22 10:48:46 +0530

    Add Go verifiers with 100 randomized tests for contest 478

---
+++ 0-999/400-499/470-479/478/verifierA.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+func run(bin, input string) (string, error) {
+	var cmd *exec.Cmd
+	if strings.HasSuffix(bin, ".go") {
+		cmd = exec.Command("go", "run", bin)
+	} else {
+		cmd = exec.Command(bin)
+	}
+	cmd.Stdin = strings.NewReader(input)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &out
+	if err := cmd.Run(); err != nil {
+		return "", fmt.Errorf("runtime error: %v\n%s", err, out.String())
+	}
+	return strings.TrimSpace(out.String()), nil
+}
+
+func expected(nums [5]int) string {
+	sum := 0
+	for _, v := range nums {
+		sum += v
+	}
+	if sum%5 != 0 || sum == 0 {
+		return "-1"
+	}
+	return fmt.Sprintf("%d", sum/5)
+}
+
+func genCase(rng *rand.Rand) (string, string) {
+	var nums [5]int
+	var sb strings.Builder
+	for i := 0; i < 5; i++ {
+		nums[i] = rng.Intn(101)
+		sb.WriteString(fmt.Sprintf("%d", nums[i]))
+		if i < 4 {
+			sb.WriteByte(' ')
+		}
+	}
+	sb.WriteByte('\n')
+	exp := expected(nums)
+	return sb.String(), exp
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "usage: go run verifierA.go /path/to/binary")
+		os.Exit(1)
+	}
+	bin := os.Args[1]
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < 100; i++ {
+		in, exp := genCase(rng)
+		out, err := run(bin, in)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "case %d failed: %v\ninput:\n%s", i+1, err, in)
+			os.Exit(1)
+		}
+		if out != exp {
+			fmt.Fprintf(os.Stderr, "case %d failed: expected %s got %s\ninput:\n%s", i+1, exp, out, in)
+			os.Exit(1)
+		}
+	}
+	fmt.Println("All tests passed")
+}
