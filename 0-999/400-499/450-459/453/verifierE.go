4bf6c0c48a3ca3f39e32fc4041ae8738813e6f38
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-22 11:13:53 +0530

    Add contest 453 verifiers (#193)

---
+++ 0-999/400-499/450-459/453/verifierE.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	_ "embed"
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+//go:embed testsE.json
+var testData []byte
+
+type Tests struct {
+	Inputs  []string `json:"inputs"`
+	Outputs []string `json:"outputs"`
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "usage: go run verifierE.go /path/to/binary")
+		os.Exit(1)
+	}
+	var t Tests
+	if err := json.Unmarshal(testData, &t); err != nil {
+		panic(err)
+	}
+	exe := os.Args[1]
+	for i, in := range t.Inputs {
+		expect := strings.TrimSpace(t.Outputs[i])
+		cmd := exec.Command(exe)
+		cmd.Stdin = strings.NewReader(in)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Printf("test %d: runtime error: %v\n", i+1, err)
+			os.Exit(1)
+		}
+		got := strings.TrimSpace(string(out))
+		if got != expect {
+			fmt.Printf("test %d failed\ninput:\n%s\nexpected:\n%s\ngot:\n%s\n", i+1, in, expect, got)
+			os.Exit(1)
+		}
+	}
+	fmt.Printf("All %d tests passed.\n", len(t.Inputs))
+}
