64fa532084f67915b65ab066ba4a1ed00f8257c7
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-22 15:43:42 +0530

    Add Go solution for 952B

---
+++ 0-999/900-999/950-959/952/952B.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	// According to the problem statement, there is no input
+	// and we simply need to state whether the cat is normal
+	fmt.Print("normal")
+}
