1369840a7c0bde8e0e641eef6220f848ebd1bed8
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-22 17:15:25 +0530

    Add Go solution for problem 984A (#669)

---
+++ 0-999/900-999/980-989/984/984A.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var n int
+	if _, err := fmt.Fscan(reader, &n); err != nil {
+		return
+	}
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(reader, &a[i])
+	}
+	sort.Ints(a)
+	fmt.Fprintln(writer, a[(n-1)/2])
+}
