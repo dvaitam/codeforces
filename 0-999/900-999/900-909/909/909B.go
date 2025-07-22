5bd9d0c85ed51802ff5d66d4ebe429225f56ef16
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-22 14:29:04 +0530

    Add Go solution for 909B

---
+++ 0-999/900-999/900-909/909/909B.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var n int
+	if _, err := fmt.Fscan(reader, &n); err != nil {
+		return
+	}
+	a := (n + 2) / 2
+	b := (n + 1) / 2
+	fmt.Fprintln(writer, a*b)
+}
