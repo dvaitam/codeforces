c975fac1ce34b20a33db63a08c97bad983337771
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-24 05:01:28 +0530

    Add Go solution for problem 640A (#1240)

---
+++ 0-999/600-699/640-649/640/640A.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var n int
+	if _, err := fmt.Fscan(reader, &n); err != nil {
+		return
+	}
+
+	result := n*(n+1)/2 + 1
+	fmt.Fprintln(writer, result)
+}
