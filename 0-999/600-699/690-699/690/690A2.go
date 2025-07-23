c081fb0767b94f155169f4fd3846e1850281a4f5
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-24 04:51:42 +0530

    Add Go solution for 690A2 (#1354)

---
+++ 0-999/600-699/690-699/690/690A2.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	var n int64
+	if _, err := fmt.Fscan(reader, &n); err != nil {
+		return
+	}
+	fmt.Println((n - 1) / 2)
+}
