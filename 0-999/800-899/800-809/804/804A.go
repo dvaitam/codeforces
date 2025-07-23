855170c20aabcdf26614e7093b2718280345d44e
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-24 04:54:31 +0530

    Add Go solution for problem 804A (#1445)

---
+++ 0-999/800-899/800-809/804/804A.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var n int
+	if _, err := fmt.Fscan(reader, &n); err != nil {
+		return
+	}
+
+	fmt.Fprintln(writer, (n-1)/2)
+}
