38784bef4960dd4c19dacf31faedd5693ab4f4b3
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-24 05:13:53 +0530

    Add Go solution for problem 679A (#1048)

---
+++ 0-999/600-699/670-679/679/679A.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func isPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	var n int
+	if _, err := fmt.Fscan(in, &n); err != nil {
+		return
+	}
+	if isPrime(n) {
+		fmt.Println("prime")
+	} else {
+		fmt.Println("composite")
+	}
+}
