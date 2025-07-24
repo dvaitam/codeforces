970bb3481fe96a894b043e90aca2f748c3d95478
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-24 20:00:15 +0530

    Add Go solution for 1768A (#2288)

---
+++ 1000-1999/1700-1799/1760-1769/1768/1768A.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	fmt.Fscan(reader, &t)
+	for ; t > 0; t-- {
+		var k int
+		fmt.Fscan(reader, &k)
+		fmt.Fprintln(writer, k-1)
+	}
+}
