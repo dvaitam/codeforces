d666a4d3f5169d7a9f943c9038eacbeca10626ac
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-24 16:00:03 +0530

    Add stub for problem 1784F (#2098)

---
+++ 1000-1999/1700-1799/1780-1789/1784/1784F.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+// TODO: implement solution
+func main() {
+    in := bufio.NewReader(os.Stdin)
+    var n, k int
+    fmt.Fscan(in, &n, &k)
+    fmt.Println(0)
+}
+
