040ce7af7b26157a4e003466e7d3a2061598ebf8
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-24 11:00:19 +0530

    Add solution for problem 784F (#1841)

---
+++ 0-999/700-799/780-789/784/784F.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var n int
+	if _, err := fmt.Fscan(reader, &n); err != nil {
+		return
+	}
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(reader, &arr[i])
+	}
+	sort.Ints(arr)
+	for i, v := range arr {
+		if i > 0 {
+			fmt.Fprint(writer, " ")
+		}
+		fmt.Fprint(writer, v)
+	}
+}
