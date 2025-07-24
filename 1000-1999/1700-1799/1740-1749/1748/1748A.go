d74ccc2f740333693b3760e862bf723f1cb08a46
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-24 16:13:36 +0530

    Add Go solution for 1748A

---
+++ 1000-1999/1700-1799/1740-1749/1748/1748A.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var t int
+	fmt.Fscan(in, &t)
+	for ; t > 0; t-- {
+		var n int64
+		fmt.Fscan(in, &n)
+		fmt.Fprintln(out, (n+1)/2)
+	}
+}
