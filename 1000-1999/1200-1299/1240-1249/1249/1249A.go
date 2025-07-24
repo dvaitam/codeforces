97e01dff309288146b31be621139a3e59936b010
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-25 02:01:38 +0530

    Add Go solution for 1249A (#2555)

---
+++ 1000-1999/1200-1299/1240-1249/1249/1249A.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var q int
+	fmt.Fscan(in, &q)
+	for ; q > 0; q-- {
+		var n int
+		fmt.Fscan(in, &n)
+		a := make([]int, n)
+		for i := 0; i < n; i++ {
+			fmt.Fscan(in, &a[i])
+		}
+		sort.Ints(a)
+		teams := 1
+		for i := 1; i < n; i++ {
+			if a[i]-a[i-1] == 1 {
+				teams = 2
+				break
+			}
+		}
+		fmt.Fprintln(out, teams)
+	}
+}
