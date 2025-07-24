3726b2a4c7e34469f97face678f5fe978f0a26ba
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-24 13:00:03 +0530

    Add Go solution for problem 1703A (#1953)

---
+++ 1000-1999/1700-1799/1700-1709/1703/1703A.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(reader, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var s string
+		fmt.Fscan(reader, &s)
+		if strings.ToUpper(s) == "YES" {
+			fmt.Fprintln(writer, "YES")
+		} else {
+			fmt.Fprintln(writer, "NO")
+		}
+	}
+}
