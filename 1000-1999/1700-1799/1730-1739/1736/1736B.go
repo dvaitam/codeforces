fae0be37717bf333ca94aa0ab7d8506ce5842cb5
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-24 18:01:37 +0530

    Add Go solution for 1736B (#2162)

---
+++ 1000-1999/1700-1799/1730-1739/1736/1736B.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func gcd(a, b int64) int64 {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+func lcm(a, b int64) int64 {
+	return a / gcd(a, b) * b
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	fmt.Fscan(reader, &t)
+	for ; t > 0; t-- {
+		var n int
+		fmt.Fscan(reader, &n)
+		a := make([]int64, n)
+		for i := 0; i < n; i++ {
+			fmt.Fscan(reader, &a[i])
+		}
+		ok := true
+		for i := 1; i < n-1; i++ {
+			l1 := lcm(a[i-1], a[i])
+			l2 := lcm(a[i], a[i+1])
+			if gcd(l1, l2) != a[i] {
+				ok = false
+				break
+			}
+		}
+		if ok {
+			fmt.Fprintln(writer, "YES")
+		} else {
+			fmt.Fprintln(writer, "NO")
+		}
+	}
+}
