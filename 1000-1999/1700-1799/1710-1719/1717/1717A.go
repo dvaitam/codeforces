72a3ea61d0d8384682089e84239deed3c0ee7b4f
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-24 20:01:36 +0530

    Add Go solution for problem 1717A (#2260)

---
+++ 1000-1999/1700-1799/1710-1719/1717/1717A.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(reader, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var n int64
+		fmt.Fscan(reader, &n)
+		ans := n + 2*(n/2) + 2*(n/3)
+		fmt.Fprintln(writer, ans)
+	}
+}
