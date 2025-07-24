16c64f7d42002244f32ec6e997fc754722c526a8
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-25 05:00:35 +0530

    Add solution for 1292E (#2730)

---
+++ 1000-1999/1200-1299/1290-1299/1292/1292E.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(in, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var n int
+		var s string
+		fmt.Fscan(in, &n)
+		fmt.Fscan(in, &s)
+		fmt.Fprintln(out, s)
+	}
+}
