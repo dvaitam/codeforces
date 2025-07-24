f96a81b8b54b01897a166ad77690b8c2e7e448c4
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-24 19:26:54 +0530

    Add placeholder solution for 1715F

---
+++ 1000-1999/1700-1799/1710-1719/1715/1715F.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// TODO: implement solution for problem F (interactive).
+// The statement provided in problemF.txt is incomplete, so this file
+// contains a placeholder solution that simply outputs 0.
+
+func main() {
+	fmt.Println(0)
+}
