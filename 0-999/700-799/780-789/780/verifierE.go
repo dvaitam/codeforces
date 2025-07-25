e5540437709c915972d4a59097dd87fa8f87e97d
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-25 22:01:43 +0530

    Add Go verifiers with random testcases for contest 780 (#3483)

---
+++ 0-999/700-799/780-789/780/verifierE.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+func runBinary(path, input string) (string, error) {
+	cmd := exec.Command(path)
+	cmd.Stdin = strings.NewReader(input)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	return strings.TrimSpace(out.String()), err
+}
+
+func generateCase() string {
+	n := rand.Intn(10) + 1
+	m := rand.Intn(n) + n // ensure connected by extra edges
+	k := rand.Intn(n) + 1
+	edges := make([][2]int, 0, m)
+	for i := 2; i <= n; i++ {
+		p := rand.Intn(i-1) + 1
+		edges = append(edges, [2]int{p, i})
+	}
+	for len(edges) < m {
+		a := rand.Intn(n) + 1
+		b := rand.Intn(n) + 1
+		edges = append(edges, [2]int{a, b})
+	}
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("%d %d %d\n", n, len(edges), k))
+	for _, e := range edges {
+		sb.WriteString(fmt.Sprintf("%d %d\n", e[0], e[1]))
+	}
+	return sb.String()
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "usage: go run verifierE.go /path/to/binary")
+		os.Exit(1)
+	}
+	target := os.Args[1]
+	rand.Seed(time.Now().UnixNano())
+
+	ref := "refE_bin"
+	if err := exec.Command("go", "build", "-o", ref, "780E.go").Run(); err != nil {
+		fmt.Fprintln(os.Stderr, "failed to build reference solution:", err)
+		os.Exit(1)
+	}
+	defer os.Remove(ref)
+
+	for i := 0; i < 100; i++ {
+		input := generateCase()
+		want, err := runBinary("./"+ref, input)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "failed to run reference solution:", err)
+			os.Exit(1)
+		}
+		got, err := runBinary(target, input)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "test %d: candidate error: %v\n", i+1, err)
+			os.Exit(1)
+		}
+		if want != got {
+			fmt.Printf("test %d failed\ninput:\n%sexpected:\n%s\ngot:\n%s\n", i+1, input, want, got)
+			os.Exit(1)
+		}
+	}
+	fmt.Println("All tests passed")
+}
---
+++ 0-999/700-799/780-789/780/verifierF.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+func runBinary(path, input string) (string, error) {
+	cmd := exec.Command(path)
+	cmd.Stdin = strings.NewReader(input)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	return strings.TrimSpace(out.String()), err
+}
+
+func generateCase() string {
+	n := rand.Intn(5) + 1
+	m := rand.Intn(n*n + 1)
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("%d %d\n", n, m))
+	for i := 0; i < m; i++ {
+		v := rand.Intn(n) + 1
+		u := rand.Intn(n) + 1
+		t := rand.Intn(2)
+		sb.WriteString(fmt.Sprintf("%d %d %d\n", v, u, t))
+	}
+	return sb.String()
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "usage: go run verifierF.go /path/to/binary")
+		os.Exit(1)
+	}
+	target := os.Args[1]
+	rand.Seed(time.Now().UnixNano())
+
+	ref := "refF_bin"
+	if err := exec.Command("go", "build", "-o", ref, "780F.go").Run(); err != nil {
+		fmt.Fprintln(os.Stderr, "failed to build reference solution:", err)
+		os.Exit(1)
+	}
+	defer os.Remove(ref)
+
+	for i := 0; i < 100; i++ {
+		input := generateCase()
+		want, err := runBinary("./"+ref, input)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "failed to run reference solution:", err)
+			os.Exit(1)
+		}
+		got, err := runBinary(target, input)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "test %d: candidate error: %v\n", i+1, err)
+			os.Exit(1)
+		}
+		if want != got {
+			fmt.Printf("test %d failed\ninput:\n%sexpected:%s\ngot:%s\n", i+1, input, want, got)
+			os.Exit(1)
+		}
+	}
+	fmt.Println("All tests passed")
+}
