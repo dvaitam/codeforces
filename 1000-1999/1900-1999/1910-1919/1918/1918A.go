05d2d29d4b477688e59d31f46f079030e5fdbad3
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-25 18:08:29 +0530

    Add Go solution for 1918A

---
+++ 1000-1999/1900-1999/1910-1919/1918/1918A.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(reader, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var n, m int
+		fmt.Fscan(reader, &n, &m)
+		fmt.Fprintln(writer, n*(m/2))
+	}
+}
