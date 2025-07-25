1e748b3fd6b99db5849f638340edfbf52378e3db
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-25 14:21:05 +0530

    Add Go solution for problem 1934A

---
+++ 1000-1999/1900-1999/1930-1939/1934/1934A.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var t int
+	fmt.Fscan(in, &t)
+	for ; t > 0; t-- {
+		var n int
+		fmt.Fscan(in, &n)
+		a := make([]int, n)
+		for i := 0; i < n; i++ {
+			fmt.Fscan(in, &a[i])
+		}
+		sort.Ints(a)
+		result := 2*(a[n-1]-a[0]) + 2*(a[n-2]-a[1])
+		fmt.Fprintln(out, result)
+	}
+}
