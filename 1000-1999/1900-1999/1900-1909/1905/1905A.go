62812bfc7b12df252e66f851b2fd257176f208ce
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-25 08:06:10 +0530

    Add solution for problem 1905A

---
+++ 1000-1999/1900-1999/1900-1909/1905/1905A.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	fmt.Fscan(reader, &t)
+	for ; t > 0; t-- {
+		var n, m int
+		fmt.Fscan(reader, &n, &m)
+		if n > m {
+			fmt.Fprintln(writer, n)
+		} else {
+			fmt.Fprintln(writer, m)
+		}
+	}
+}
