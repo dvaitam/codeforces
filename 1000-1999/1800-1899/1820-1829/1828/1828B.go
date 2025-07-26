1b0e4eef3acf690b8d3e4909f56f4ca777524563
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-27 00:02:00 +0530

    Add solution for 1828B

---
+++ 1000-1999/1800-1899/1820-1829/1828/1828B.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(in, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var n int
+		fmt.Fscan(in, &n)
+		arr := make([]int, n)
+		for i := 0; i < n; i++ {
+			fmt.Fscan(in, &arr[i])
+		}
+		g := 0
+		for i := 0; i < n; i++ {
+			diff := arr[i] - (i + 1)
+			if diff < 0 {
+				diff = -diff
+			}
+			g = gcd(g, diff)
+		}
+		fmt.Fprintln(out, g)
+	}
+}
