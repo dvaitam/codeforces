61d1548b19c2b59c34e8966e69d10da9697f0e89
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-26 18:00:29 +0530

    Add solution for 1839A (#4466)

---
+++ 1000-1999/1800-1899/1830-1839/1839/1839A.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	fmt.Fscan(reader, &t)
+	for ; t > 0; t-- {
+		var n, k int
+		fmt.Fscan(reader, &n, &k)
+		ans := (n + 2*(k-1)) / k
+		fmt.Fprintln(writer, ans)
+	}
+}
