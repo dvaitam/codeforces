935273a1899387bb29236d27d3ecd16032be05bc
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-27 00:16:08 +0530

    Add stub solution for 1821F

---
+++ 1000-1999/1800-1899/1820-1829/1821/1821F.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+const mod int = 998244353
+
+func main() {
+    in := bufio.NewReader(os.Stdin)
+    var n, m, k int
+    fmt.Fscan(in, &n, &m, &k)
+    // TODO: implement the actual counting algorithm
+    fmt.Println(0)
+}
+
