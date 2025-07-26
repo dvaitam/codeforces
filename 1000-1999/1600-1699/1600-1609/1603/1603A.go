a05cc36ee096797030c1feadc7119ef9054515d4
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-26 06:58:31 +0530

    Add Go solution for problem 1603A

---
+++ 1000-1999/1600-1699/1600-1609/1603/1603A.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func gcd(a, b int64) int64 {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+func lcm(a, b int64) int64 {
+	g := gcd(a, b)
+	return a / g * b
+}
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(in, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var n int
+		fmt.Fscan(in, &n)
+		a := make([]int64, n)
+		for i := 0; i < n; i++ {
+			fmt.Fscan(in, &a[i])
+		}
+		l := int64(1)
+		ok := true
+		for i, v := range a {
+			l = lcm(l, int64(i+2))
+			if l > 1_000_000_000 {
+				break
+			}
+			if v%l == 0 {
+				ok = false
+				break
+			}
+		}
+		if ok {
+			fmt.Fprintln(out, "YES")
+		} else {
+			fmt.Fprintln(out, "NO")
+		}
+	}
+}
