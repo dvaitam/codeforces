d196227d8727f45320129b73ac2716c559f2914a
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-26 11:00:29 +0530

    Add Go solution for 1651A (#4120)

---
+++ 1000-1999/1600-1699/1650-1659/1651/1651A.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(reader, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var n int
+		fmt.Fscan(reader, &n)
+		res := (1 << n) - 1
+		fmt.Fprintln(writer, res)
+	}
+}
