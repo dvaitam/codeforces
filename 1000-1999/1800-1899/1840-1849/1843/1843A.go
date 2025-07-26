957ca7a5f3ff003363dfad807f862571778698c8
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-26 20:28:47 +0530

    Add Go solution for 1843A

---
+++ 1000-1999/1800-1899/1840-1849/1843/1843A.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(in, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var n int
+		fmt.Fscan(in, &n)
+		a := make([]int, n)
+		for i := 0; i < n; i++ {
+			fmt.Fscan(in, &a[i])
+		}
+		sort.Ints(a)
+		ans := 0
+		for i := 0; i < n/2; i++ {
+			ans += a[n-1-i] - a[i]
+		}
+		fmt.Fprintln(out, ans)
+	}
+}
