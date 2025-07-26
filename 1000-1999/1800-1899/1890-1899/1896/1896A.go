53f9818ee4b035a614b80b1003c1d61c5888fc90
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-27 04:00:22 +0530

    Add Go solution for 1896A (#4983)

---
+++ 1000-1999/1800-1899/1890-1899/1896/1896A.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	fmt.Fscan(reader, &t)
+	for ; t > 0; t-- {
+		var n int
+		fmt.Fscan(reader, &n)
+		a := make([]int, n)
+		for i := 0; i < n; i++ {
+			fmt.Fscan(reader, &a[i])
+		}
+		if a[0] == 1 {
+			fmt.Fprintln(writer, "YES")
+		} else {
+			fmt.Fprintln(writer, "NO")
+		}
+	}
+}
