68b293a6adb6c77699b02b0444a6be83865941a1
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-26 21:00:51 +0530

    Add Go solution for 1844A (#4612)

---
+++ 1000-1999/1800-1899/1840-1849/1844/1844A.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+	var t int
+	if _, err := fmt.Fscan(in, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var a, b int
+		fmt.Fscan(in, &a, &b)
+		fmt.Fprintln(out, a+b)
+	}
+}
