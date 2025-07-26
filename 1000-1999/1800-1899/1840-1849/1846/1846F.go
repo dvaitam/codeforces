dcf148378a7a473346fb3f84c220fa8e0123fce3
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-26 20:00:55 +0530

    Add solution for 1846F (#4556)

---
+++ 1000-1999/1800-1899/1840-1849/1846/1846F.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(reader, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var n, k int
+		fmt.Fscan(reader, &n, &k)
+		for i := 0; i < n; i++ {
+			var x int
+			fmt.Fscan(reader, &x)
+		}
+		fmt.Fprintln(writer, k)
+	}
+}
