51e950158e3e9db5626fe6e710d7ab1f81978a64
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-27 02:55:31 +0530

    Add placeholder solution for 1812B

---
+++ 1000-1999/1800-1899/1810-1819/1812/1812B.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	var n int
+	if _, err := fmt.Fscan(reader, &n); err != nil {
+		return
+	}
+	fmt.Println(n)
+}
