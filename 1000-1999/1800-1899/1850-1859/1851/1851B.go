94397cad7b840392e66ff0835936861864d53bf5
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-27 01:00:07 +0530

    Add Go solution for problem 1851B (#4835)

---
+++ 1000-1999/1800-1899/1850-1859/1851/1851B.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(in, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var n int
+		fmt.Fscan(in, &n)
+		a := make([]int, n)
+		for i := 0; i < n; i++ {
+			fmt.Fscan(in, &a[i])
+		}
+		b := make([]int, n)
+		copy(b, a)
+		sort.Ints(b)
+		ok := true
+		for i := 0; i < n; i++ {
+			if a[i]%2 != b[i]%2 {
+				ok = false
+				break
+			}
+		}
+		if ok {
+			fmt.Fprintln(out, "YES")
+		} else {
+			fmt.Fprintln(out, "NO")
+		}
+	}
+}
