a12cc1544c602378b72de550048d9cfd5866e213
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-27 04:01:59 +0530

    Add Go solution for 1810A (#4945)

---
+++ 1000-1999/1800-1899/1810-1819/1810/1810A.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(reader, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var n int
+		fmt.Fscan(reader, &n)
+		ok := false
+		for i := 1; i <= n; i++ {
+			var x int
+			fmt.Fscan(reader, &x)
+			if x <= i {
+				ok = true
+			}
+		}
+		if ok {
+			fmt.Fprintln(writer, "YES")
+		} else {
+			fmt.Fprintln(writer, "NO")
+		}
+	}
+}
