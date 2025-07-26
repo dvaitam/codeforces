0427bac45b979d9f7440f3d560251dcde5cb1f8c
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-26 21:01:01 +0530

    Add solution for problem 1843C (#4608)

---
+++ 1000-1999/1800-1899/1840-1849/1843/1843C.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(reader, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var n int64
+		fmt.Fscan(reader, &n)
+		var sum int64
+		for n > 0 {
+			sum += n
+			n /= 2
+		}
+		fmt.Fprintln(writer, sum)
+	}
+}
