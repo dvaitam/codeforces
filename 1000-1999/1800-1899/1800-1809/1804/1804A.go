6cab55372f67432842b2bd6b19a65975b71dde8b
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-26 23:43:23 +0530

    Add Go solution for 1804A

---
+++ 1000-1999/1800-1899/1800-1809/1804/1804A.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(reader, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var a, b int
+		fmt.Fscan(reader, &a, &b)
+		res := abs(a) + abs(b)
+		fmt.Fprintln(writer, res)
+	}
+}
