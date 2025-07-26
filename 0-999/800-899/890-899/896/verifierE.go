28cdeaa1d82603309053c52a36bc88e8c1e14326
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-26 08:00:37 +0530

    Add Go verifiers for contest 896 (#3983)

---
+++ 0-999/800-899/890-899/896/verifierE.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"time"
+)
+
+const numTests = 100
+
+func buildRef(dir string) (string, error) {
+	ref := filepath.Join(dir, "refE_bin")
+	cmd := exec.Command("go", "build", "-o", ref, "896E.go")
+	cmd.Dir = dir
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("build reference failed: %v\n%s", err, string(out))
+	}
+	return ref, nil
+}
+
+func runBinary(bin string, input []byte) (string, error) {
+	cmd := exec.Command(bin)
+	cmd.Stdin = bytes.NewReader(input)
+	out, err := cmd.CombinedOutput()
+	return string(out), err
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run verifierE.go /path/to/binary")
+		os.Exit(1)
+	}
+	target := os.Args[1]
+	rand.Seed(time.Now().UnixNano())
+	_, filename, _, _ := runtime.Caller(0)
+	dir := filepath.Dir(filename)
+
+	ref, err := buildRef(dir)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer os.Remove(ref)
+
+	for t := 1; t <= numTests; t++ {
+		n := rand.Intn(10) + 1
+		m := rand.Intn(10) + 1
+		var input bytes.Buffer
+		input.WriteString(fmt.Sprintf("%d %d\n", n, m))
+		arr := make([]int, n)
+		for i := 0; i < n; i++ {
+			arr[i] = rand.Intn(10) + 1
+			input.WriteString(fmt.Sprintf("%d ", arr[i]))
+		}
+		input.WriteString("\n")
+		for i := 0; i < m; i++ {
+			typ := rand.Intn(2) + 1
+			l := rand.Intn(n) + 1
+			r := rand.Intn(n-l+1) + l
+			x := rand.Intn(10) + 1
+			input.WriteString(fmt.Sprintf("%d %d %d %d\n", typ, l, r, x))
+		}
+		inBytes := input.Bytes()
+
+		out, err := runBinary(target, inBytes)
+		if err != nil {
+			fmt.Printf("Test %d: runtime error: %v\n", t, err)
+			os.Exit(1)
+		}
+		refOut, err := runBinary(ref, inBytes)
+		if err != nil {
+			fmt.Printf("Test %d: reference runtime error: %v\n", t, err)
+			os.Exit(1)
+		}
+		if strings.TrimSpace(out) != strings.TrimSpace(refOut) {
+			fmt.Printf("Test %d failed\nInput:\n%sExpected:\n%sGot:\n%s\n", t, input.String(), refOut, out)
+			os.Exit(1)
+		}
+	}
+	fmt.Printf("Passed %d tests\n", numTests)
+}
