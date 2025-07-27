8faa3e4dcfa052dfc2a6b1a40750b42ac8d06f39
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-27 05:38:23 +0530

    Add Go solution for 1347A

---
+++ 1000-1999/1300-1399/1340-1349/1347/1347A.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(reader, &t); err != nil {
+		return
+	}
+
+	for i := 0; i < t; i++ {
+		var a, b int
+		fmt.Fscan(reader, &a, &b)
+		fmt.Fprintln(writer, a+b)
+	}
+}
