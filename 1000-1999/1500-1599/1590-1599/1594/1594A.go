7970ae5e64b4f08c11db4ef18fab5962fd7597da
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-27 17:01:30 +0530

    Add Go solution for 1594A (#5555)

---
+++ 1000-1999/1500-1599/1590-1599/1594/1594A.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	fmt.Fscan(reader, &t)
+	for ; t > 0; t-- {
+		var n int64
+		fmt.Fscan(reader, &n)
+		fmt.Fprintf(writer, "%d %d\n", -n+1, n)
+	}
+}
