508ad6655a4a172be2da1e5673582bfa80095daa
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-27 15:02:05 +0530

    Add Go verifiers for contest 1360 (#5435)

---
+++ 1000-1999/1300-1399/1360-1369/1360/verifierE.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func run(path, input string) (string, error) {
+	cmd := exec.Command(path)
+	cmd.Stdin = strings.NewReader(input)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	return strings.TrimSpace(out.String()), err
+}
+
+func buildRef() (string, error) {
+	ref := "./refE.bin"
+	cmd := exec.Command("go", "build", "-o", ref, "1360E.go")
+	if out, err := cmd.CombinedOutput(); err != nil {
+		return "", fmt.Errorf("failed to build reference: %v: %s", err, string(out))
+	}
+	return ref, nil
+}
+
+func genTests() []string {
+	rand.Seed(5)
+	tests := make([]string, 100)
+	for i := range tests {
+		n := rand.Intn(10) + 1
+		var sb strings.Builder
+		sb.WriteString("1\n")
+		sb.WriteString(fmt.Sprintf("%d\n", n))
+		for r := 0; r < n; r++ {
+			for c := 0; c < n; c++ {
+				if rand.Intn(2) == 1 {
+					sb.WriteByte('1')
+				} else {
+					sb.WriteByte('0')
+				}
+			}
+			sb.WriteByte('\n')
+		}
+		tests[i] = sb.String()
+	}
+	return tests
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("usage: go run verifierE.go /path/to/binary")
+		return
+	}
+	candidate := os.Args[1]
+	ref, err := buildRef()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer os.Remove(ref)
+
+	tests := genTests()
+	for i, t := range tests {
+		exp, err := run(ref, t)
+		if err != nil {
+			fmt.Printf("reference runtime error on test %d: %v\n", i+1, err)
+			return
+		}
+		out, err := run(candidate, t)
+		if err != nil {
+			fmt.Printf("candidate runtime error on test %d: %v\n", i+1, err)
+			return
+		}
+		if exp != out {
+			fmt.Printf("wrong answer on test %d\nexpected: %s\ngot: %s\n", i+1, exp, out)
+			return
+		}
+	}
+	fmt.Println("All tests passed!")
+}
