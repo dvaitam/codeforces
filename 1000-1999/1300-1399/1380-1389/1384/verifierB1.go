6111e0c668714a841273ede64987a27828caaa7b
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-27 15:00:08 +0530

    Add Go verifiers for Codeforces contest 1384 (#5480)

---
+++ 1000-1999/1300-1399/1380-1389/1384/verifierB1.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+type caseB struct {
+	n int
+	k int
+	l int
+	d []int
+}
+
+func generateTests() []caseB {
+	r := rand.New(rand.NewSource(2))
+	tests := []caseB{
+		{2, 1, 1, []int{0, 1}},
+		{1, 1, 2, []int{1}},
+	}
+	for len(tests) < 120 {
+		n := r.Intn(8) + 1
+		k := r.Intn(5) + 1
+		l := r.Intn(10) + 1
+		d := make([]int, n)
+		for i := 0; i < n; i++ {
+			d[i] = r.Intn(l + k + 1)
+		}
+		tests = append(tests, caseB{n, k, l, d})
+	}
+	return tests
+}
+
+func runCandidate(bin, input string) (string, error) {
+	var cmd *exec.Cmd
+	if strings.HasSuffix(bin, ".go") {
+		cmd = exec.Command("go", "run", bin)
+	} else {
+		cmd = exec.Command(bin)
+	}
+	cmd.Stdin = strings.NewReader(input)
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	if err := cmd.Run(); err != nil {
+		return "", fmt.Errorf("runtime error: %v\n%s", err, stderr.String())
+	}
+	return strings.TrimSpace(out.String()), nil
+}
+
+func solveCase(n, k, l int, d []int) bool {
+	p := make([]int, 0, 2*k)
+	for i := 0; i < k; i++ {
+		p = append(p, i)
+	}
+	p = append(p, k)
+	for i := k - 1; i >= 1; i-- {
+		p = append(p, i)
+	}
+	cycle := len(p)
+	type state struct{ pos, tm int }
+	queue := []state{{0, 0}}
+	vis := make([][]bool, n+2)
+	for i := range vis {
+		vis[i] = make([]bool, cycle)
+	}
+	vis[0][0] = true
+	for idx := 0; idx < len(queue); idx++ {
+		cur := queue[idx]
+		pos, tm := cur.pos, cur.tm
+		if pos == n+1 {
+			return true
+		}
+		nt := (tm + 1) % cycle
+		if pos == 0 || pos == n+1 {
+			if !vis[pos][nt] {
+				vis[pos][nt] = true
+				queue = append(queue, state{pos, nt})
+			}
+		} else if d[pos-1]+p[nt] <= l {
+			if !vis[pos][nt] {
+				vis[pos][nt] = true
+				queue = append(queue, state{pos, nt})
+			}
+		}
+		np := pos + 1
+		if np == n+1 {
+			if !vis[np][nt] {
+				vis[np][nt] = true
+				queue = append(queue, state{np, nt})
+			}
+		} else if np <= n && d[np-1]+p[nt] <= l {
+			if !vis[np][nt] {
+				vis[np][nt] = true
+				queue = append(queue, state{np, nt})
+			}
+		}
+	}
+	return false
+}
+
+func verify(tc caseB, out string) error {
+	ans := "No"
+	if solveCase(tc.n, tc.k, tc.l, tc.d) {
+		ans = "Yes"
+	}
+	out = strings.TrimSpace(out)
+	if strings.EqualFold(out, ans) {
+		return nil
+	}
+	return fmt.Errorf("expected %s, got %s", ans, out)
+}
+
+func main() {
+	var bin string
+	if len(os.Args) == 2 {
+		bin = os.Args[1]
+	} else if len(os.Args) == 3 && os.Args[1] == "--" {
+		bin = os.Args[2]
+	} else {
+		fmt.Fprintln(os.Stderr, "usage: go run verifierB1.go /path/to/binary")
+		os.Exit(1)
+	}
+	tests := generateTests()
+	for i, tc := range tests {
+		var sb strings.Builder
+		sb.WriteString("1\n")
+		fmt.Fprintf(&sb, "%d %d %d\n", tc.n, tc.k, tc.l)
+		for j, v := range tc.d {
+			if j > 0 {
+				sb.WriteByte(' ')
+			}
+			sb.WriteString(strconv.Itoa(v))
+		}
+		sb.WriteByte('\n')
+		out, err := runCandidate(bin, sb.String())
+		if err != nil {
+			fmt.Printf("runtime error on test %d: %v\n", i+1, err)
+			os.Exit(1)
+		}
+		if err := verify(tc, out); err != nil {
+			fmt.Printf("wrong answer on test %d: %v\n", i+1, err)
+			os.Exit(1)
+		}
+	}
+	fmt.Printf("All %d tests passed.\n", len(tests))
+}
