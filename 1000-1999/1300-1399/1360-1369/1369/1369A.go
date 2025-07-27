53f169566e210fa0a0699f69352dbe988912b450
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-27 09:07:28 +0530

    Add Go solution for problem 1369A

---
+++ 1000-1999/1300-1399/1360-1369/1369/1369A.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(in, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var n int
+		fmt.Fscan(in, &n)
+		if n%4 == 0 {
+			fmt.Fprintln(out, "YES")
+		} else {
+			fmt.Fprintln(out, "NO")
+		}
+	}
+}
