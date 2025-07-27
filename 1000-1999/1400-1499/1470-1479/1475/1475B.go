0b3c8f166f0e4ad3f22b2c01951f49c4718c2022
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-28 02:37:11 +0530

    Add Go solution for 1475B

---
+++ 1000-1999/1400-1499/1470-1479/1475/1475B.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(reader, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var n int
+		fmt.Fscan(reader, &n)
+		if n%2020 <= n/2020 {
+			fmt.Fprintln(writer, "YES")
+		} else {
+			fmt.Fprintln(writer, "NO")
+		}
+	}
+}
