066095c25acbd131c3e44577204f266c917a46c1
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-27 06:02:45 +0530

    Add Go solution for problem 1388B

---
+++ 1000-1999/1300-1399/1380-1389/1388/1388B.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(reader, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var n int
+		fmt.Fscan(reader, &n)
+		m := (n + 3) / 4
+		result := strings.Repeat("9", n-m) + strings.Repeat("8", m)
+		fmt.Fprintln(writer, result)
+	}
+}
