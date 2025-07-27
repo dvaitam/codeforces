c2eaca5c188bfa94817bb5c76751c4c8e04a1b9d
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-27 10:01:28 +0530

    Add Go solution for 1360A Minimal Square (#5268)

---
+++ 1000-1999/1300-1399/1360-1369/1360/1360A.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(reader, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var a, b int
+		fmt.Fscan(reader, &a, &b)
+		if a > b {
+			a, b = b, a
+		}
+		side := b
+		if 2*a > b {
+			side = 2 * a
+		}
+		fmt.Fprintln(writer, side*side)
+	}
+}
