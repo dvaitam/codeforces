cc45c853946b5a855c3a128a24b3cf704285b187
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-27 09:17:26 +0530

    Add solution for problem 1358B

---
+++ 1000-1999/1300-1399/1350-1359/1358/1358B.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(reader, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var n int
+		fmt.Fscan(reader, &n)
+		a := make([]int, n)
+		for i := 0; i < n; i++ {
+			fmt.Fscan(reader, &a[i])
+		}
+		sort.Ints(a)
+		ans := 1
+		for i := 0; i < n; i++ {
+			if a[i] <= i+2 {
+				ans = i + 2
+			}
+		}
+		fmt.Fprintln(writer, ans)
+	}
+}
