967048c38ad46ce99cc122db4938b0d4be28c565
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-27 23:04:55 +0530

    Add Go solution for problem 1549A

---
+++ 1000-1999/1500-1599/1540-1549/1549/1549A.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var t int
+	if _, err := fmt.Fscan(in, &t); err != nil {
+		return
+	}
+	for ; t > 0; t-- {
+		var p int64
+		fmt.Fscan(in, &p)
+		fmt.Fprintf(out, "2 %d\n", p-1)
+	}
+}
