f4e7bd67928b81a5c031821e0d0f44f822f91915
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-28 13:43:33 +0530

    Add Go verifiers and tests for contest 1556

---
+++ 1000-1999/1500-1599/1550-1559/1556/verifierF.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("usage: go run verifierF.go /path/to/binary")
+		os.Exit(1)
+	}
+	bin := os.Args[1]
+	cases, _ := filepath.Glob("tests/F/*.in")
+	total := 0
+	for _, inFile := range cases {
+		outFile := strings.TrimSuffix(inFile, ".in") + ".out"
+		inData, _ := ioutil.ReadFile(inFile)
+		want, _ := ioutil.ReadFile(outFile)
+		cmd := exec.Command(bin)
+		cmd.Stdin = bytes.NewReader(inData)
+		got, err := cmd.Output()
+		if err != nil {
+			fmt.Printf("%s: runtime error: %v\n", inFile, err)
+			os.Exit(1)
+		}
+		if !bytes.Equal(bytes.TrimSpace(got), bytes.TrimSpace(want)) {
+			fmt.Printf("%s: wrong answer\nwant: %q\ngot:  %q\n", inFile, want, got)
+			os.Exit(1)
+		}
+		total++
+	}
+	fmt.Printf("OK %d cases\n", total)
+}
