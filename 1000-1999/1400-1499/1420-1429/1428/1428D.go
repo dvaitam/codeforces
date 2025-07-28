490c06e5fc53f5398274fcca21cb1cdfd713644c
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-28 14:57:45 +0530

    Update 1428D.go

--- 1000-1999/1400-1499/1420-1429/1428/1428D.go
+++ 1000-1999/1400-1499/1420-1429/1428/1428D.go
@@ -1,64 +1,87 @@
 package main
 
 import (
-   "bufio"
-   "fmt"
-   "os"
+	"bufio"
+	"fmt"
+	"os"
 )
 
-func main() {
-   reader := bufio.NewReader(os.Stdin)
-   writer := bufio.NewWriter(os.Stdout)
-   defer writer.Flush()
+type pair struct {
+	r, c int
+}
 
-   var n int
-   if _, err := fmt.Fscan(reader, &n); err != nil {
-       return
-   }
-   q := make([]int, 0, n)
-   head := 0
-   res := make([][2]int, 0, 2*n)
-   r := 0
-   pre := 0
-   for i := 1; i <= n; i++ {
-       var a int
-       fmt.Fscan(reader, &a)
-       if a == 0 {
-           continue
-       }
-       if a == 1 {
-           if head < len(q) {
-               res = append(res, [2]int{q[head], i})
-               head++
-           } else {
-               if pre == 3 {
-                   res = append(res, [2]int{r, i})
-               }
-               r++
-               res = append(res, [2]int{r, i})
-           }
-       } else if a == 2 {
-           if pre == 3 {
-               res = append(res, [2]int{r, i})
-           }
-           r++
-           res = append(res, [2]int{r, i})
-           q = append(q, r)
-       } else if a == 3 {
-           if pre == 3 {
-               res = append(res, [2]int{r, i})
-           }
-           r++
-           res = append(res, [2]int{r, i})
-       }
-       pre = a
-   }
-   if head < len(q) || pre == 3 {
-       fmt.Fprintln(writer, -1)
-       return
-   }
-   fmt.Fprintln(writer, len(res))
-   for _, p := range res {
-       fmt.Fprintln(writer, p[0], p[1])
-   }
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	var n int
+	if _, err := fmt.Fscan(in, &n); err != nil {
+		return
+	}
+	a := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		fmt.Fscan(in, &a[i])
+	}
+
+	row := n
+	ans := make([]pair, 0, 2*n)
+	ones := make([]pair, 0) // columns with a==1, waiting to be paired
+	twos := make([]pair, 0) // columns that already have two hits, can serve for third
+
+	ok := true
+	for col := n; col >= 1 && ok; col-- {
+		switch a[col] {
+		case 0:
+			// nothing to do
+		case 1:
+			if row == 0 {
+				ok = false
+				break
+			}
+			ans = append(ans, pair{row, col})
+			ones = append(ones, pair{row, col})
+			row--
+		case 2:
+			if len(ones) == 0 {
+				ok = false
+				break
+			}
+			p := ones[len(ones)-1]
+			ones = ones[:len(ones)-1]
+			ans = append(ans, pair{p.r, col})
+			twos = append(twos, pair{p.r, col})
+		case 3:
+			if row == 0 {
+				ok = false
+				break
+			}
+			var p pair
+			if len(twos) > 0 {
+				p = twos[len(twos)-1]
+				twos = twos[:len(twos)-1]
+			} else if len(ones) > 0 {
+				p = ones[len(ones)-1]
+				ones = ones[:len(ones)-1]
+			} else {
+				ok = false
+				break
+			}
+			ans = append(ans, pair{row, col})
+			ans = append(ans, pair{row, p.c})
+			twos = append(twos, pair{row, col})
+			row--
+		default:
+			ok = false
+		}
+	}
+
+	if !ok {
+		fmt.Println(-1)
+		return
+	}
+
+	out := bufio.NewWriter(os.Stdout)
+	fmt.Fprintln(out, len(ans))
+	for _, pr := range ans {
+		fmt.Fprintf(out, "%d %d\n", pr.r, pr.c)
+	}
+	out.Flush()
 }
