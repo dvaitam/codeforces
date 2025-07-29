28f440ec09abd58a3f23808071000855e8947c1e
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-29 12:05:55 +0530

    Add spacing below submit button

--- webserver.go
+++ webserver.go
@@ -100,6 +100,7 @@ textarea { width: 100%; }
 <textarea name="code" rows="20" cols="80"></textarea><br>
 <input type="file" name="file"><br>
 <input type="submit" value="Submit">
+<div style="padding-bottom:20px;"></div>
 </form>
 </body></html>`))
 
