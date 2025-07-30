37ebc141f1e7b4448c812cbfd9b6b9902ab2dda9
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-07-30 17:49:28 +0530

    Update 1264C.go

--- 1000-1999/1200-1299/1260-1269/1264/1264C.go
+++ 1000-1999/1200-1299/1260-1269/1264/1264C.go
@@ -3,229 +3,241 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math/rand"
 	"os"
+	"strconv"
 )
 
-const MOD int64 = 998244353
+// --- Global Variables ---
+var (
+	n, q    int
+	invP    []int64
+	segTree []Node
+)
 
-// treap for ordered set
-type node struct {
-	key   int
-	prio  int
-	left  *node
-	right *node
+// --- Constants and I/O ---
+const M = 998244353
+
+var scanner *bufio.Scanner
+var writer *bufio.Writer
+
+func init() {
+	scanner = bufio.NewScanner(os.Stdin)
+	// Increase buffer size for large inputs
+	scanner.Buffer(make([]byte, 1024*1024), 1024*1024)
+	scanner.Split(bufio.ScanWords)
+	writer = bufio.NewWriter(os.Stdout)
 }
 
-func split(root *node, key int) (l, r *node) {
-	if root == nil {
-		return nil, nil
-	}
-	if root.key < key {
-		root.right, r = split(root.right, key)
-		l = root
-	} else {
-		l, root.left = split(root.left, key)
-		r = root
-	}
-	return
+func nextInt() int {
+	scanner.Scan()
+	i, _ := strconv.Atoi(scanner.Text())
+	return i
 }
 
-func merge(l, r *node) *node {
-	if l == nil {
-		return r
-	}
-	if r == nil {
-		return l
-	}
-	if l.prio > r.prio {
-		l.right = merge(l.right, r)
-		return l
-	}
-	r.left = merge(l, r.left)
-	return r
+func writeln(a ...interface{}) {
+	fmt.Fprintln(writer, a...)
 }
 
-func insertNode(root *node, nd *node) *node {
-	if root == nil {
-		return nd
-	}
-	if nd.prio > root.prio {
-		nd.left, nd.right = split(root, nd.key)
-		return nd
-	}
-	if nd.key < root.key {
-		root.left = insertNode(root.left, nd)
-	} else {
-		root.right = insertNode(root.right, nd)
-	}
-	return root
-}
-
-func deleteNode(root *node, key int) *node {
-	if root == nil {
-		return nil
-	}
-	if root.key == key {
-		return merge(root.left, root.right)
-	}
-	if key < root.key {
-		root.left = deleteNode(root.left, key)
-	} else {
-		root.right = deleteNode(root.right, key)
-	}
-	return root
-}
-
-func predecessor(root *node, key int) int {
-	res := -1 << 60
-	for root != nil {
-		if root.key < key {
-			if root.key > res {
-				res = root.key
-			}
-			root = root.right
-		} else {
-			root = root.left
-		}
-	}
-	return res
-}
-
-func successor(root *node, key int) int {
-	res := 1<<60 - 1
-	for root != nil {
-		if root.key > key {
-			if root.key < res {
-				res = root.key
-			}
-			root = root.left
-		} else {
-			root = root.right
-		}
-	}
-	return res
-}
-
-type orderedSet struct{ root *node }
-
-func (s *orderedSet) insert(key int)   { s.root = insertNode(s.root, &node{key: key, prio: rand.Int()}) }
-func (s *orderedSet) remove(key int)   { s.root = deleteNode(s.root, key) }
-func (s *orderedSet) prev(key int) int { return predecessor(s.root, key) }
-func (s *orderedSet) next(key int) int { return successor(s.root, key) }
-
-// modular exponentiation
-func powmod(a, e int64) int64 {
+// --- Modular Arithmetic ---
+func power(a, b int64) int64 {
 	res := int64(1)
-	a %= MOD
-	for e > 0 {
-		if e&1 == 1 {
-			res = res * a % MOD
+	a %= M
+	for b > 0 {
+		if b%2 == 1 {
+			res = (res * a) % M
 		}
-		a = a * a % MOD
-		e >>= 1
+		a = (a * a) % M
+		b /= 2
 	}
 	return res
 }
+
+func modInverse(n int64) int64 {
+	return power(n, M-2)
+}
+
+// --- Segment Tree ---
+type Node struct {
+	val  int64
+	prod int64
+}
+
+// merge combines two adjacent segments [L, M] and [M+1, R]
+func merge(left, right Node) Node {
+	// The formula for combining segments is:
+	// val(L, R+1) = val(L, M+1) * prod(M+1, R+1) + val(M+1, R+1)
+	newVal := (left.val*right.prod + right.val) % M
+	newProd := (left.prod * right.prod) % M
+	return Node{val: newVal, prod: newProd}
+}
+
+func buildSegTree(v, tl, tr int) {
+	if tl == tr {
+		segTree[v] = Node{val: invP[tl], prod: invP[tl]}
+	} else {
+		tm := (tl + tr) / 2
+		buildSegTree(v*2, tl, tm)
+		buildSegTree(v*2+1, tm+1, tr)
+		segTree[v] = merge(segTree[v*2], segTree[v*2+1])
+	}
+}
+
+func querySegTree(v, tl, tr, l, r int) Node {
+	if l > r {
+		return Node{val: 0, prod: 1} // Identity element
+	}
+	if l == tl && r == tr {
+		return segTree[v]
+	}
+	tm := (tl + tr) / 2
+	if r <= tm {
+		return querySegTree(v*2, tl, tm, l, r)
+	}
+	if l > tm {
+		return querySegTree(v*2+1, tm+1, tr, l, r)
+	}
+	leftResult := querySegTree(v*2, tl, tm, l, tm)
+	rightResult := querySegTree(v*2+1, tm+1, tr, tm+1, r)
+	return merge(leftResult, rightResult)
+}
+
+// --- BIT (Fenwick Tree) ---
+type BIT struct {
+	size int
+	tree []int
+}
+
+func newBIT(size int) *BIT {
+	return &BIT{
+		size: size,
+		tree: make([]int, size+1),
+	}
+}
+
+func (b *BIT) add(idx, delta int) {
+	for ; idx <= b.size; idx += idx & -idx {
+		b.tree[idx] += delta
+	}
+}
+
+func (b *BIT) prefixSum(idx int) int {
+	if idx < 1 {
+		return 0
+	}
+	sum := 0
+	for ; idx > 0; idx -= idx & -idx {
+		sum += b.tree[idx]
+	}
+	return sum
+}
+
+// findKth finds the index of the k-th checkpoint (k is 1-based)
+func (b *BIT) findKth(k int) int {
+	if k <= 0 {
+		return 0
+	}
+	pos := 0
+	sum := 0
+	
+	logN := 0
+	for (1 << (logN + 1)) <= b.size {
+		logN++
+	}
+
+	for i := logN; i >= 0; i-- {
+		p := 1 << i
+		if pos+p <= b.size && sum+b.tree[pos+p] < k {
+			sum += b.tree[pos+p]
+			pos += p
+		}
+	}
+	return pos + 1
+}
+
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	writer := bufio.NewWriter(os.Stdout)
 	defer writer.Flush()
 
-	var n, q int
-	if _, err := fmt.Fscan(reader, &n, &q); err != nil {
-		return
-	}
+	n = nextInt()
+	q = nextInt()
 
 	p := make([]int64, n+1)
 	for i := 1; i <= n; i++ {
-		var x int64
-		fmt.Fscan(reader, &x)
-		p[i] = x
+		p[i] = int64(nextInt())
 	}
 
-	inv100 := powmod(100, MOD-2)
-
-	prob := make([]int64, n+1)
+	invP = make([]int64, n+1)
 	for i := 1; i <= n; i++ {
-		prob[i] = p[i] * inv100 % MOD
+		invP[i] = (100 * modInverse(p[i])) % M
 	}
 
-	pref := make([]int64, n+1)
-	pref[0] = 1
-	for i := 1; i <= n; i++ {
-		pref[i] = pref[i-1] * prob[i] % MOD
-	}
+	segTree = make([]Node, 4*(n+1))
+	buildSegTree(1, 1, n)
 
-	invPref := make([]int64, n+1)
-	for i := 0; i <= n; i++ {
-		invPref[i] = powmod(pref[i], MOD-2)
-	}
+	checkpointBIT := newBIT(n)
+	isCheckpoint := make(map[int]bool)
 
-	sumInv := make([]int64, n+1)
-	for i := 1; i <= n; i++ {
-		sumInv[i] = (sumInv[i-1] + invPref[i]) % MOD
-	}
-
-	segExp := func(l, r int) int64 {
-		if l >= r {
-			return 0
-		}
-		t := sumInv[r-1] - sumInv[l-1]
-		t %= MOD
-		if t < 0 {
-			t += MOD
-		}
-		return pref[l-1] * t % MOD
-	}
-
-	st := &orderedSet{}
-	st.insert(1)
-	st.insert(n + 1)
-	inSet := make(map[int]bool)
-	inSet[1] = true
-	inSet[n+1] = true
-
-	ans := segExp(1, n+1)
+	// Initial state: only mirror 1 is a checkpoint
+	isCheckpoint[1] = true
+	checkpointBIT.add(1, 1)
+	totalE := querySegTree(1, 1, n, 1, n).val
 
 	for i := 0; i < q; i++ {
-		var u int
-		fmt.Fscan(reader, &u)
-		if !inSet[u] {
-			// insert u
-			l := st.prev(u)
-			r := st.next(u)
-			ans -= segExp(l, r)
-			ans %= MOD
-			if ans < 0 {
-				ans += MOD
+		u := nextInt()
+
+		var cp, cn int
+		isUCheckpoint := isCheckpoint[u]
+		
+		numCheckpoints := checkpointBIT.prefixSum(n)
+
+		if isUCheckpoint { // Case: removing u from checkpoints
+			rankU := checkpointBIT.prefixSum(u)
+			
+			// Find previous checkpoint
+			cpRank := rankU - 1
+			cp = checkpointBIT.findKth(cpRank)
+			
+			// Find next checkpoint
+			nextRank := rankU + 1
+			if nextRank > numCheckpoints {
+				cn = n + 1
+			} else {
+				cn = checkpointBIT.findKth(nextRank)
 			}
-			ans += segExp(l, u)
-			ans %= MOD
-			ans += segExp(u, r)
-			ans %= MOD
-			st.insert(u)
-			inSet[u] = true
-		} else {
-			// remove u
-			l := st.prev(u)
-			r := st.next(u)
-			ans -= segExp(l, u)
-			ans %= MOD
-			if ans < 0 {
-				ans += MOD
+		} else { // Case: adding u to checkpoints
+			rankBeforeU := checkpointBIT.prefixSum(u - 1)
+
+			// Find previous checkpoint
+			cpRank := rankBeforeU
+			// Since u > 1, cpRank >= 1 and a checkpoint must exist.
+			cp = checkpointBIT.findKth(cpRank)
+
+			// Find next checkpoint
+			nextRank := rankBeforeU + 1
+			if nextRank > numCheckpoints {
+				cn = n + 1
+			} else {
+				cn = checkpointBIT.findKth(nextRank)
 			}
-			ans -= segExp(u, r)
-			ans %= MOD
-			if ans < 0 {
-				ans += MOD
-			}
-			ans += segExp(l, r)
-			ans %= MOD
-			st.remove(u)
-			delete(inSet, u)
 		}
-		fmt.Fprintln(writer, ans)
+
+		// Calculate change in total expected value
+		// Change is val(cp, u) * (1 - prod(u, cn))
+		valCP_U_node := querySegTree(1, 1, n, cp, u-1)
+		prodU_CN_node := querySegTree(1, 1, n, u, cn-1)
+
+		delta := (valCP_U_node.val * (1 - prodU_CN_node.prod + M)) % M
+		
+		if isUCheckpoint {
+			totalE = (totalE - delta + M) % M
+			checkpointBIT.add(u, -1)
+			isCheckpoint[u] = false
+		} else {
+			totalE = (totalE + delta) % M
+			checkpointBIT.add(u, 1)
+			isCheckpoint[u] = true
+		}
+		
+		writeln(totalE)
 	}
 }
