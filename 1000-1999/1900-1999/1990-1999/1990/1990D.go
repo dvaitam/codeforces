eb968d2f621e3f72b4507743c2b20d733531183a
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-08-02 17:37:18 +0530

    Update 1990D.go

--- 1000-1999/1900-1999/1990-1999/1990/1990D.go
+++ 1000-1999/1900-1999/1990-1999/1990/1990D.go
@@ -6,42 +6,59 @@ import (
 	"os"
 )
 
+const N = 200005
+
+func solve(scanner *bufio.Scanner, writer *bufio.Writer) {
+	scanner.Scan()
+	var n int
+	fmt.Sscan(scanner.Text(), &n)
+	
+	a := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		scanner.Scan()
+		fmt.Sscan(scanner.Text(), &a[i])
+		if a[i]%2 == 1 {
+			a[i]++
+		}
+	}
+	
+	t := 0
+	for i := 1; i <= n; i++ {
+		if a[i] == 4 && a[i-1] == 4 {
+			a[i], a[i-1] = 2, 2
+			t++
+		}
+	}
+	
+	for i := 1; i <= n; i++ {
+		if a[i] == 2 && a[i-1] == 2 {
+			a[i], a[i-1] = 0, 0
+			t++
+		}
+	}
+	
+	for i := 1; i <= n; i++ {
+		if a[i] != 0 {
+			t++
+		}
+	}
+	
+	fmt.Fprintln(writer, t)
+}
+
 func main() {
-	reader := bufio.NewReader(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 	writer := bufio.NewWriter(os.Stdout)
 	defer writer.Flush()
-
-	const maxN = 200005
-
+	
+	scanner.Split(bufio.ScanWords)
+	
+	scanner.Scan()
 	var T int
-	fmt.Fscan(reader, &T)
-	for ; T > 0; T-- {
-		var n int64
-		fmt.Fscan(reader, &n)
-		var b, c int64
-		sum := int64(0)
-		p1 := make([]bool, maxN)
-		p2 := make([]bool, maxN)
-		for n > 0 {
-			n--
-			var x int64
-			fmt.Fscan(reader, &x)
-			if p1[x] {
-				if x > b {
-					b = x
-				}
-			} else {
-				p1[x] = true
-			}
-			if p2[b] {
-				if b > c {
-					c = b
-				}
-			} else if b != 0 {
-				p2[b] = true
-			}
-			sum += x + b + (n+1)*c
-		}
-		fmt.Fprintln(writer, sum)
+	fmt.Sscan(scanner.Text(), &T)
+	
+	for T > 0 {
+		solve(scanner, writer)
+		T--
 	}
 }
