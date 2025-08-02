1e350022f142326e0887966e1193d1274942717d
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-08-02 16:44:32 +0530

    Update 1991B.go

--- 1000-1999/1900-1999/1990-1999/1991/1991B.go
+++ 1000-1999/1900-1999/1990-1999/1991/1991B.go
@@ -1,53 +1,40 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
 )
 
+const N = 1000003
+
 func main() {
-    in := bufio.NewReader(os.Stdin)
-    out := bufio.NewWriter(os.Stdout)
-    defer out.Flush()
-
-    var t int
-    fmt.Fscan(in, &t)
-    for ; t > 0; t-- {
-        var n int
-        fmt.Fscan(in, &n)
-        b := make([]int, n-1)
-        for i := 0; i < n-1; i++ {
-            fmt.Fscan(in, &b[i])
-        }
-        possible := true
-        for i := 1; i < n-1 && possible; i++ {
-            if ((b[i-1] & b[i+1]) &^ b[i]) != 0 {
-                possible = false
-            }
-        }
-        if !possible {
-            fmt.Fprintln(out, -1)
-            continue
-        }
-        a := make([]int, n)
-        if n == 1 {
-            // not reachable since b length is n-1 but handle
-            a[0] = 0
-        } else {
-            a[0] = b[0]
-            for i := 1; i < n-1; i++ {
-                a[i] = b[i-1] | b[i]
-            }
-            a[n-1] = b[n-2]
-        }
-        for i := 0; i < n; i++ {
-            if i > 0 {
-                fmt.Fprint(out, " ")
-            }
-            fmt.Fprint(out, a[i])
-        }
-        fmt.Fprintln(out)
-    }
+	in := bufio.NewReader(os.Stdin)
+	var T int
+	fmt.Fscan(in, &T)
+	for ; T > 0; T-- {
+		var n int
+		fmt.Fscan(in, &n)
+		a := make([]int64, n)
+		b := make([]int64, n+1)
+		for i := 1; i < n; i++ {
+			fmt.Fscan(in, &a[i])
+		}
+		b[n] = a[n-1]
+		b[1] = a[1]
+		for i := 2; i < n; i++ {
+			b[i] = a[i-1] | a[i]
+		}
+		for i := 1; i < n; i++ {
+			if b[i]&b[i+1] != a[i] {
+				fmt.Println(-1)
+				goto end
+			}
+		}
+		for i := 1; i <= n; i++ {
+			fmt.Print(b[i], " ")
+		}
+		fmt.Println()
+	end:
+	}
 }
-
