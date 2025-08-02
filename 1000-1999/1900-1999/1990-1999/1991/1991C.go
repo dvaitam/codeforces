25fbd4a18c211daf4370e459065edc6f6b50232b
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-08-02 16:44:56 +0530

    Update 1991C.go

--- 1000-1999/1900-1999/1990-1999/1991/1991C.go
+++ 1000-1999/1900-1999/1990-1999/1991/1991C.go
@@ -1,33 +1,44 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func main() {
-    in := bufio.NewReader(os.Stdin)
-    out := bufio.NewWriter(os.Stdout)
-    defer out.Flush()
-
-    var t int
-    fmt.Fscan(in, &t)
-    for ; t > 0; t-- {
-        var n int
-        fmt.Fscan(in, &n)
-        zero := true
-        for i := 0; i < n; i++ {
-            var x int
-            fmt.Fscan(in, &x)
-            if x != 0 {
-                zero = false
-            }
-        }
-        if zero {
-            fmt.Fprintln(out, 0)
-        } else {
-            fmt.Fprintln(out, -1)
-        }
-    }
+	in := bufio.NewReader(os.Stdin)
+	var T, n int
+	fmt.Fscan(in, &T)
+	for ; T > 0; T-- {
+		fmt.Fscan(in, &n)
+		a := make([]int, n+1)
+		for i := 1; i <= n; i++ {
+			fmt.Fscan(in, &a[i])
+		}
+		k, kk := 0, 0
+		for i := 1; i <= n; i++ {
+			if a[i]%2 == 0 {
+				k = 1
+			} else {
+				kk = 1
+			}
+		}
+		if k*kk == 1 {
+			fmt.Println(-1)
+			continue
+		}
+		if kk == 1 {
+			fmt.Println(30)
+		} else {
+			fmt.Println(31)
+		}
+		for i := 29; i >= 0; i-- {
+			fmt.Print(1<<i, " ")
+		}
+		if k == 1 {
+			fmt.Print(1, " ")
+		}
+		fmt.Println()
+	}
 }
