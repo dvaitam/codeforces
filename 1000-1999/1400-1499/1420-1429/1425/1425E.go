85ef7e661d7d5fb37b2cdffbd8f22994bb9d530a
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-08-11 19:18:12 +0530

    Fix 1425E reference implementation (#6763)

--- 1000-1999/1400-1499/1420-1429/1425/1425E.go
+++ 1000-1999/1400-1499/1420-1429/1425/1425E.go
@@ -1,81 +1,74 @@
 package main
 
 import (
-   "bufio"
-   "fmt"
-   "os"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func main() {
-   in := bufio.NewReader(os.Stdin)
-   var N, K int
-   fmt.Fscan(in, &N, &K)
-   A := make([]int64, N+1)
-   D := make([]int64, N+1)
-   for i := 1; i <= N; i++ {
-       fmt.Fscan(in, &A[i])
-   }
-   for i := 1; i <= N; i++ {
-       fmt.Fscan(in, &D[i])
-   }
-   // prefix sums
-   ps := make([]int64, N+1)
-   for i := 1; i <= N; i++ {
-       ps[i] = ps[i-1] + A[i]
-   }
-   // helper to run DP with penalty C per segment
-   run := func(C int64) (cnt int, dpVal int64) {
-       var dpPrev int64 = 0
-       var cntPrev int = 0
-       // best M = max(dp[j-1] - ps[j-1] - D[j])
-       var bestM int64 = -1<<60
-       var bestCnt int = 0
-       for r := 1; r <= N; r++ {
-           // update M for segment starting at r
-           m := dpPrev - ps[r-1] - D[r]
-           if m > bestM || (m == bestM && cntPrev < bestCnt) {
-               bestM = m
-               bestCnt = cntPrev
-           }
-           // consider ending segment at r
-           raw := ps[r] + bestM
-           var dpCur int64
-           var cntCur int
-           cand := raw - C
-           if cand > dpPrev {
-               dpCur = cand
-               cntCur = bestCnt + 1
-           } else if cand < dpPrev {
-               dpCur = dpPrev
-               cntCur = cntPrev
-           } else {
-               dpCur = dpPrev
-               // tie: choose fewer segments
-               if cntPrev < bestCnt+1 {
-                   cntCur = cntPrev
-               } else {
-                   cntCur = bestCnt + 1
-               }
-           }
-           dpPrev = dpCur
-           cntPrev = cntCur
-       }
-       return cntPrev, dpPrev
-   }
-   // binary search penalty C >=0
-   var l, r int64 = 0, ps[N]
-   for l < r {
-       mid := (l + r + 1) >> 1
-       cnt, _ := run(mid)
-       if cnt >= K+1 {
-           l = mid
-       } else {
-           r = mid - 1
-       }
-   }
-   C := l
-   _, dpv := run(C)
-   // result is dpv + C*(K+1)
-   res := dpv + C*int64(K+1)
-   fmt.Println(res)
+	in := bufio.NewReader(os.Stdin)
+	var n, d int64
+	fmt.Fscan(in, &n, &d)
+	a := make([]int64, n+1)
+	wg := make([]int64, n+1)
+	ps := make([]int64, n+1)
+	for i := int64(1); i <= n; i++ {
+		fmt.Fscan(in, &a[i])
+		ps[i] = ps[i-1] + a[i]
+	}
+	for i := int64(1); i <= n; i++ {
+		fmt.Fscan(in, &wg[i])
+	}
+	sx := make([]int64, n+2)
+	for i := n; i >= 1; i-- {
+		val := ps[n] - ps[i-1] - wg[i]
+		if sx[i+1] > val {
+			val = sx[i+1]
+		}
+		sx[i] = val
+	}
+	z0 := sx[1]
+	const inf int64 = 1 << 60
+	mn := inf
+	w := inf
+	var z1 int64
+	for i := int64(1); i < n; i++ {
+		if wg[i] < mn {
+			mn = wg[i]
+		}
+		pre := ps[i] - mn
+		if pre < 0 {
+			pre = 0
+		}
+		cand := sx[i+1] + pre
+		if cand > z1 {
+			z1 = cand
+		}
+		if i > 1 {
+			v := a[i]
+			if wg[i] < v {
+				v = wg[i]
+			}
+			if v < w {
+				w = v
+			}
+		}
+	}
+	cand1 := ps[n] - wg[1] - w
+	if cand1 > z1 {
+		z1 = cand1
+	}
+	z2 := ps[n] - mn
+	tmp := a[n] - wg[n]
+	if tmp > z2 {
+		z2 = tmp
+	}
+	if d == 0 {
+		fmt.Println(z0)
+	} else if d == 1 {
+		fmt.Println(z1)
+	} else {
+		fmt.Println(z2)
+	}
 }
