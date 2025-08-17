bc51a30300570a3c5ff8313c78fff76c1e57aa5f
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-08-17 12:16:45 +0530

    Fix 1987D verifier and solution (#6829)

--- 1000-1999/1900-1999/1980-1989/1987/1987D.go
+++ 1000-1999/1900-1999/1980-1989/1987/1987D.go
@@ -2,10 +2,50 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"fmt"
 	"os"
-	"sort"
 )
+
+type maxHeap []int
+
+func (h maxHeap) Len() int           { return len(h) }
+func (h maxHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h maxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *maxHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+func (h *maxHeap) Pop() interface{} {
+	old := *h
+	x := old[len(old)-1]
+	*h = old[:len(old)-1]
+	return x
+}
+
+func solve(a []int) int {
+	n := len(a)
+	freq := make([]int, n+1)
+	for _, v := range a {
+		freq[v]++
+	}
+	h := &maxHeap{}
+	heap.Init(h)
+	s := 0
+	sum := 0
+	for v := 1; v <= n; v++ {
+		f := freq[v]
+		if f > 0 {
+			heap.Push(h, f)
+			sum += f
+			for sum > s {
+				big := heap.Pop(h).(int)
+				sum -= big
+				s++
+			}
+		}
+	}
+	return s
+}
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
@@ -13,34 +53,14 @@ func main() {
 	defer writer.Flush()
 
 	var t int
-	if _, err := fmt.Fscan(reader, &t); err != nil {
-		return
-	}
+	fmt.Fscan(reader, &t)
 	for ; t > 0; t-- {
 		var n int
 		fmt.Fscan(reader, &n)
 		a := make([]int, n)
-		for i := 0; i < n; i++ {
+		for i := range a {
 			fmt.Fscan(reader, &a[i])
 		}
-		sort.Ints(a)
-		prev := -1
-		left, right := 0, n-1
-		eaten := 0
-		for left <= right {
-			for left <= right && a[left] <= prev {
-				left++
-			}
-			if left > right {
-				break
-			}
-			prev = a[left]
-			left++
-			eaten++
-			if left <= right {
-				right--
-			}
-		}
-		fmt.Fprintln(writer, eaten)
+		fmt.Fprintln(writer, solve(a))
 	}
 }
