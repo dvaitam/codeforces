44b1726ad3fc7330bed7eeb5e06822bbeaa8ab74
Anil M <amanchikatla@icloud.com>
2025-08-20 16:29:00 +0530

    update verifierC for 1437

--- 1000-1999/1400-1499/1430-1439/1437/1437C.go
+++ 1000-1999/1400-1499/1430-1439/1437/1437C.go
@@ -1,72 +1,41 @@
 package main
 
 import (
-   "bufio"
-   "fmt"
-   "os"
-   "sort"
+    "fmt"
+    "sort"
 )
 
-func abs(x int) int {
-   if x < 0 {
-       return -x
-   }
-   return x
+func abs(x int) int { if x < 0 { return -x }; return x }
+
+const infinity = 1 << 30
+
+func solve() {
+    var n int
+    fmt.Scan(&n)
+    t := make([]int, n)
+    for i := 0; i < n; i++ { fmt.Scan(&t[i]) }
+    sort.Ints(t)
+
+    maxTime := 2*n + 5
+    if n == 0 { maxTime = 5 }
+    dp := make([][]int, n+1)
+    for i := range dp {
+        dp[i] = make([]int, maxTime)
+        for j := range dp[i] { dp[i][j] = infinity }
+    }
+    for j := 0; j < maxTime; j++ { dp[0][j] = 0 }
+    for i := 1; i <= n; i++ {
+        for j := 1; j < maxTime; j++ {
+            val1 := dp[i][j-1]
+            val2 := dp[i-1][j-1] + abs(t[i-1]-j)
+            if val1 < val2 { dp[i][j] = val1 } else { dp[i][j] = val2 }
+        }
+    }
+    fmt.Println(dp[n][maxTime-1])
 }
 
 func main() {
-   in := bufio.NewReader(os.Stdin)
-   var q int
-   if _, err := fmt.Fscan(in, &q); err != nil {
-       return
-   }
-   out := bufio.NewWriter(os.Stdout)
-   defer out.Flush()
-   for q > 0 {
-       q--
-       var n int
-       fmt.Fscan(in, &n)
-       t := make([]int, n)
-       for i := 0; i < n; i++ {
-           fmt.Fscan(in, &t[i])
-       }
-       sort.Ints(t)
-       maxT := 2 * n
-       const INF = 1_000_000_000
-       // dpPrev[j]: min unpleasant for previous i-1 dishes ending at time j
-       dpPrev := make([]int, maxT+1)
-       for j := 1; j <= maxT; j++ {
-           dpPrev[j] = INF
-       }
-       // dpPrev[0] = 0 implicitly
-       for i := 1; i <= n; i++ {
-           // prefix minimum of dpPrev
-           prefix := make([]int, maxT+1)
-           prefix[0] = dpPrev[0]
-           for j := 1; j <= maxT; j++ {
-               if dpPrev[j] < prefix[j-1] {
-                   prefix[j] = dpPrev[j]
-               } else {
-                   prefix[j] = prefix[j-1]
-               }
-           }
-           dpCur := make([]int, maxT+1)
-           for j := 1; j <= maxT; j++ {
-               // choose previous time k < j
-               best := prefix[j-1]
-               dpCur[j] = best + abs(j - t[i-1])
-           }
-           // no valid assignment ending at time 0 for i>0
-           dpCur[0] = INF
-           dpPrev = dpCur
-       }
-       // answer is min over dpPrev[j]
-       ans := INF
-       for j := 1; j <= 2*n; j++ {
-           if dpPrev[j] < ans {
-               ans = dpPrev[j]
-           }
-       }
-       fmt.Fprintln(out, ans)
-   }
+    var q int
+    fmt.Scan(&q)
+    for i := 0; i < q; i++ { solve() }
 }
