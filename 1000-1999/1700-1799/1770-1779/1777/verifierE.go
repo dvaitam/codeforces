5a501251459ffbf348d2addab0ae3d819b99e05e
Anil M <amanchikatla@icloud.com>
2025-08-26 21:47:12 +0530

    updated verifierE for 1777

--- 1000-1999/1700-1799/1770-1779/1777/verifierE.go
+++ 1000-1999/1700-1799/1770-1779/1777/verifierE.go
@@ -10,122 +10,85 @@ import (
 	"time"
 )
 
-type dsu struct {
-	p  []int
-	sz []int
-}
-
-func newDSU(n int) *dsu {
-	d := &dsu{p: make([]int, n), sz: make([]int, n)}
-	for i := 0; i < n; i++ {
-		d.p[i] = i
-		d.sz[i] = 1
-	}
-	return d
-}
-
-func (d *dsu) find(x int) int {
-	if d.p[x] != x {
-		d.p[x] = d.find(d.p[x])
-	}
-	return d.p[x]
-}
-
-func (d *dsu) union(a, b int) {
-	a = d.find(a)
-	b = d.find(b)
-	if a == b {
-		return
-	}
-	if d.sz[a] < d.sz[b] {
-		a, b = b, a
-	}
-	d.p[b] = a
-	d.sz[a] += d.sz[b]
-}
-
 type Edge struct {
-	u, v int
-	w    int
+    u, v int
+    w    int
 }
 
-func can(edges []Edge, n int, x int) bool {
-	d := newDSU(n)
-	for _, e := range edges {
-		if e.w <= x {
-			d.union(e.u, e.v)
-		}
-	}
-	compID := make(map[int]int)
-	id := 0
-	for i := 0; i < n; i++ {
-		r := d.find(i)
-		if _, ok := compID[r]; !ok {
-			compID[r] = id
-			id++
-		}
-	}
-	indeg := make([]int, id)
-	for _, e := range edges {
-		if e.w > x {
-			a := compID[d.find(e.u)]
-			b := compID[d.find(e.v)]
-			if a != b {
-				indeg[b]++
-			}
-		}
-	}
-	cnt := 0
-	for i := 0; i < id; i++ {
-		if indeg[i] == 0 {
-			cnt++
-			if cnt > 1 {
-				return false
-			}
-		}
-	}
-	return cnt == 1
+// Build directed adjacency: for each input edge (u,v,w), add u->v cost 0 and v->u cost w.
+func buildAdj(n int, edges []Edge) [][]struct{ to, c int } {
+    adj := make([][]struct{ to, c int }, n)
+    for _, e := range edges {
+        adj[e.u] = append(adj[e.u], struct{ to, c int }{e.v, 0})
+        adj[e.v] = append(adj[e.v], struct{ to, c int }{e.u, e.w})
+    }
+    return adj
+}
+
+func dfsFrom(adj [][]struct{ to, c int }, k int, start int, vis []bool) int {
+    stack := []int{start}
+    vis[start] = true
+    cnt := 0
+    for len(stack) > 0 {
+        x := stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        cnt++
+        for _, e := range adj[x] {
+            if e.c <= k && !vis[e.to] {
+                vis[e.to] = true
+                stack = append(stack, e.to)
+            }
+        }
+    }
+    return cnt
+}
+
+// Check if the directed graph (with edges allowed when cost<=k) has a mother vertex.
+func motherExists(n int, adj [][]struct{ to, c int }, k int) bool {
+    vis := make([]bool, n)
+    last := 0
+    for i := 0; i < n; i++ {
+        if !vis[i] {
+            dfsFrom(adj, k, i, vis)
+            last = i
+        }
+    }
+    for i := range vis {
+        vis[i] = false
+    }
+    return dfsFrom(adj, k, last, vis) == n
 }
 
 func solveCase(n, m int, edges []Edge) int {
-	d := newDSU(n)
-	for _, e := range edges {
-		d.union(e.u, e.v)
-	}
-	root := d.find(0)
-	for i := 1; i < n; i++ {
-		if d.find(i) != root {
-			return -1
-		}
-	}
-	lo, hi := 0, int(1e9)
-	for lo < hi {
-		mid := (lo + hi) / 2
-		if can(edges, n, mid) {
-			hi = mid
-		} else {
-			lo = mid + 1
-		}
-	}
-	return lo
+    adj := buildAdj(n, edges)
+    lo, hi := 0, int(1e9)+10
+    for lo < hi {
+        mid := (lo + hi) >> 1
+        if motherExists(n, adj, mid) {
+            hi = mid
+        } else {
+            lo = mid + 1
+        }
+    }
+    if lo > int(1e9) {
+        return -1
+    }
+    return lo
 }
 
 func generateCase(rng *rand.Rand) (string, string) {
-	n := rng.Intn(5) + 1
-	m := rng.Intn(5) + n - 1
+    n := rng.Intn(5) + 1
+    m := rng.Intn(5) + n - 1
 	edges := make([]Edge, m)
-	// start with tree
-	for i := 0; i < n-1; i++ {
-		edges[i] = Edge{u: i, v: i + 1, w: rng.Intn(20)}
-	}
-	for i := n - 1; i < m; i++ {
-		u := rng.Intn(n)
-		v := rng.Intn(n)
-		for u == v {
-			v = rng.Intn(n)
-		}
-		edges[i] = Edge{u: u, v: v, w: rng.Intn(20)}
-	}
+    // random edges; allow zero weights too
+    for i := 0; i < m; i++ {
+        u := rng.Intn(n)
+        v := rng.Intn(n)
+        for u == v {
+            v = rng.Intn(n)
+        }
+        edges[i] = Edge{u: u, v: v, w: rng.Intn(21)}
+    }
 	ans := solveCase(n, m, edges)
 	var sb strings.Builder
 	sb.WriteString("1\n")
