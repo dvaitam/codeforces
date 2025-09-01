90c57d69cff7f7840ef38d05196dc0b3ab7db575
Anil M <amanchikatla@icloud.com>
2025-09-01 19:42:19 +0530

    update verifierF for 1670

--- 1000-1999/1600-1699/1670-1679/1670/verifierF.go
+++ 1000-1999/1600-1699/1670-1679/1670/verifierF.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 )
 
@@ -22,13 +23,91 @@ func run(cmdPath string, input string) (string, error) {
 	return strings.TrimSpace(out.String()), err
 }
 
-func buildRef() (string, error) {
-	ref := "./refF.bin"
-	cmd := exec.Command("go", "build", "-o", ref, "1670F.go")
-	if out, err := cmd.CombinedOutput(); err != nil {
-		return "", fmt.Errorf("build reference failed: %v: %s", err, string(out))
+// semantic oracle for 1670F
+const mod1670 = int64(1000000007)
+
+func modPow1670(a, e int64) int64 {
+	res := int64(1)
+	for e > 0 {
+		if e&1 == 1 {
+			res = res * a % mod1670
+		}
+		a = a * a % mod1670
+		e >>= 1
 	}
-	return ref, nil
+	return res
+}
+
+func countUpTo(n int, x, z int64, choose []int64) int64 {
+	if x < 0 {
+		return 0
+	}
+	maxBit := 61
+	dp := make([][2]int64, n+1)
+	dp[0][0] = 1
+	for i := 0; i < maxBit; i++ {
+		xi := int((x >> i) & 1)
+		zi := int((z >> i) & 1)
+		next := make([][2]int64, n+1)
+		for c := 0; c <= n; c++ {
+			for less := 0; less < 2; less++ {
+				ways := dp[c][less]
+				if ways == 0 {
+					continue
+				}
+				bitSum := (c & 1) ^ zi
+				if less == 0 && bitSum > xi {
+					continue
+				}
+				start := (c - bitSum + 1) / 2
+				if start < 0 {
+					start = 0
+				}
+				end := (c + n - bitSum) / 2
+				if end > n {
+					end = n
+				}
+				newLessBase := less
+				if less == 0 && bitSum < xi {
+					newLessBase = 1
+				}
+				for cp := start; cp <= end; cp++ {
+					k := 2*cp + bitSum - c
+					if k < 0 || k > n {
+						continue
+					}
+					val := ways * choose[k] % mod1670
+					next[cp][newLessBase] = (next[cp][newLessBase] + val) % mod1670
+				}
+			}
+		}
+		dp = next
+	}
+	return (dp[0][0] + dp[0][1]) % mod1670
+}
+
+func expected1670F(n int, l, r, z int64) int64 {
+	// precompute C(n, k)
+	fact := make([]int64, n+1)
+	invFact := make([]int64, n+1)
+	fact[0] = 1
+	for i := 1; i <= n; i++ {
+		fact[i] = fact[i-1] * int64(i) % mod1670
+	}
+	invFact[n] = modPow1670(fact[n], mod1670-2)
+	for i := n; i > 0; i-- {
+		invFact[i-1] = invFact[i] * int64(i) % mod1670
+	}
+	choose := make([]int64, n+1)
+	for k := 0; k <= n; k++ {
+		choose[k] = fact[n] * invFact[k] % mod1670 * invFact[n-k] % mod1670
+	}
+	ans := countUpTo(n, r, z, choose) - countUpTo(n, l-1, z, choose)
+	ans %= mod1670
+	if ans < 0 {
+		ans += mod1670
+	}
+	return ans
 }
 
 func genTests() []string {
@@ -54,27 +133,37 @@ func main() {
 		return
 	}
 	bin := os.Args[1]
-	ref, err := buildRef()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	defer os.Remove(ref)
-
 	tests := genTests()
 	for i, tc := range tests {
-		exp, err := run(ref, tc)
-		if err != nil {
-			fmt.Printf("reference runtime error on test %d: %v\n", i+1, err)
+		// compute expected
+		fields := strings.Fields(tc)
+		if len(fields) < 4 {
+			fmt.Printf("bad test %d: %q\n", i+1, tc)
 			os.Exit(1)
 		}
-		got, err := run(bin, tc)
+		n, _ := strconv.Atoi(fields[0])
+		l, _ := strconv.ParseInt(fields[1], 10, 64)
+		r, _ := strconv.ParseInt(fields[2], 10, 64)
+		z, _ := strconv.ParseInt(fields[3], 10, 64)
+		expVal := expected1670F(n, l, r, z)
+		gotStr, err := run(bin, tc)
 		if err != nil {
 			fmt.Printf("candidate runtime error on test %d: %v\n", i+1, err)
 			os.Exit(1)
 		}
-		if exp != got {
-			fmt.Printf("test %d failed\ninput:\n%sexpected:%s\ngot:%s\n", i+1, tc, exp, got)
+		gotStr = strings.TrimSpace(gotStr)
+		gotFields := strings.Fields(gotStr)
+		if len(gotFields) == 0 {
+			fmt.Printf("test %d failed\ninput:\n%sexpected:%d\ngot:%q\n", i+1, tc, expVal, gotStr)
+			os.Exit(1)
+		}
+		gotVal, err := strconv.ParseInt(gotFields[0], 10, 64)
+		if err != nil {
+			fmt.Printf("test %d failed\ninput:\n%sexpected:%d\ngot:%q\n", i+1, tc, expVal, gotStr)
+			os.Exit(1)
+		}
+		if gotVal != expVal {
+			fmt.Printf("test %d failed\ninput:\n%sexpected:%d\ngot:%d\n", i+1, tc, expVal, gotVal)
 			os.Exit(1)
 		}
 	}
