669bcf1bef7f356ca9f2f560488275445f0da884
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-11-22 17:31:12 +0530

    Fix 641G oracle to match correct solution

--- 0-999/600-699/640-649/641/641G.go
+++ 0-999/600-699/640-649/641/641G.go
@@ -4,65 +4,25 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 )
 
 const MOD = 1000000007
 
-func power(a, b int64) int64 {
-	var res int64 = 1
-	a %= MOD
+func powMod(a, b int) int {
+	res := 1
+	base := int64(a)
 	for b > 0 {
 		if b&1 == 1 {
-			res = (res * a) % MOD
+			res = int((int64(res) * base) % MOD)
 		}
-		a = (a * a) % MOD
+		base = (base * base) % MOD
 		b >>= 1
 	}
 	return res
 }
 
-func modInverse(n int64) int64 {
-	return power(n, MOD-2)
-}
-
-func gaussianDet(a [][]int64, n int) int64 {
-	var res int64 = 1
-	mat := make([][]int64, n)
-	for i := 0; i < n; i++ {
-		mat[i] = make([]int64, n)
-		copy(mat[i], a[i])
-	}
-
-	for i := 0; i < n; i++ {
-		pivot := i
-		for j := i + 1; j < n; j++ {
-			if mat[j][i] != 0 {
-				pivot = j
-				break
-			}
-		}
-		if mat[pivot][i] == 0 {
-			return 0
-		}
-		if pivot != i {
-			mat[i], mat[pivot] = mat[pivot], mat[i]
-			res = (MOD - res) % MOD
-		}
-		res = (res * mat[i][i]) % MOD
-		inv := modInverse(mat[i][i])
-
-		for j := i + 1; j < n; j++ {
-			if mat[j][i] != 0 {
-				factor := (mat[j][i] * inv) % MOD
-				for l := i; l < n; l++ {
-					sub := (factor * mat[i][l]) % MOD
-					mat[j][l] = (mat[j][l] - sub + MOD) % MOD
-				}
-			}
-		}
-	}
-	return res
+func modInverse(n int) int {
+	return powMod(n, MOD-2)
 }
 
 func main() {
@@ -71,99 +31,96 @@ func main() {
 
 	scanInt := func() int {
 		scanner.Scan()
-		val, _ := strconv.Atoi(scanner.Text())
-		return val
+		s := scanner.Text()
+		x := 0
+		for i := 0; i < len(s); i++ {
+			x = x*10 + int(s[i]-'0')
+		}
+		return x
 	}
 
-	n := scanInt()
-	k := scanInt()
-
-	neighbors := make([][]int, n+1)
-	for i := k + 1; i <= n; i++ {
-		neighbors[i] = make([]int, k)
-		for j := 0; j < k; j++ {
-			neighbors[i][j] = scanInt()
-		}
-	}
-
-	adj := make([]map[int]int64, n+1)
-	for i := 1; i <= n; i++ {
-		adj[i] = make(map[int]int64)
-	}
-
-	for i := 1; i <= k; i++ {
-		for j := i + 1; j <= k; j++ {
-			adj[i][j] = 1
-			adj[j][i] = 1
-		}
-	}
-	for i := k + 1; i <= n; i++ {
-		for _, u := range neighbors[i] {
-			adj[i][u] = 1
-			adj[u][i] = 1
-		}
-	}
-
-	var ans int64 = 1
-
-	for i := n; i > k; i-- {
-		var D int64 = 0
-		for _, u := range neighbors[i] {
-			D = (D + adj[i][u]) % MOD
-		}
-
-		ans = (ans * D) % MOD
-		invD := modInverse(D)
-
-		ns := neighbors[i]
-		for a := 0; a < k; a++ {
-			u := ns[a]
-			wu := adj[i][u]
-			for b := a + 1; b < k; b++ {
-				v := ns[b]
-				wv := adj[i][v]
-
-				term := (wu * wv) % MOD
-				term = (term * invD) % MOD
-
-				old := adj[u][v]
-				newVal := (old + term) % MOD
-				adj[u][v] = newVal
-				adj[v][u] = newVal
-			}
-		}
-
-		adj[i] = nil
-		for _, u := range neighbors[i] {
-			delete(adj[u], i)
-		}
-	}
-
-	if k == 1 {
-		fmt.Println(ans)
+	if !scanner.Scan() {
 		return
 	}
-
-	mat := make([][]int64, k)
-	for i := range mat {
-		mat[i] = make([]int64, k)
+	nStr := scanner.Text()
+	n := 0
+	for i := 0; i < len(nStr); i++ {
+		n = n*10 + int(nStr[i]-'0')
 	}
 
+	k := scanInt()
+
+	diag := make([]int, n+1)
+	mat := make([]map[int]int, n+1)
+	for i := 1; i <= n; i++ {
+		mat[i] = make(map[int]int)
+	}
+
+	// Initialize initial clique (vertices 1 to k)
 	for i := 1; i <= k; i++ {
-		var sum int64 = 0
-		for j := 1; j <= k; j++ {
-			if i == j {
-				continue
-			}
-			w := adj[i][j]
-			mat[i-1][j-1] = (MOD - w) % MOD
-			sum = (sum + w) % MOD
+		diag[i] = k - 1
+		for j := 1; j < i; j++ {
+			mat[i][j] = MOD - 1
 		}
-		mat[i-1][i-1] = sum
 	}
 
-	det := gaussianDet(mat, k-1)
-	ans = (ans * det) % MOD
+	// Read vertices k+1 to n
+	for i := k + 1; i <= n; i++ {
+		diag[i] = k
+		for j := 0; j < k; j++ {
+			v := scanInt()
+			// Edge between i and v (v < i)
+			mat[i][v] = MOD - 1
+			diag[v]++
+		}
+	}
+
+	ans := int64(1)
+
+	// Eliminate vertices from n down to 2
+	for i := n; i >= 2; i-- {
+		d := diag[i] % MOD
+		if d < 0 {
+			d += MOD
+		}
+		ans = (ans * int64(d)) % MOD
+
+		inv := modInverse(d)
+
+		neighbors := make([]int, 0, len(mat[i]))
+		for u := range mat[i] {
+			neighbors = append(neighbors, u)
+		}
+
+		for idx, u := range neighbors {
+			valU := mat[i][u]
+
+			// Update diag[u]
+			term := int((int64(valU) * int64(valU)) % MOD)
+			term = int((int64(term) * int64(inv)) % MOD)
+			diag[u] = (diag[u] - term + MOD) % MOD
+
+			// Update edges between neighbors
+			for j := 0; j < idx; j++ {
+				v := neighbors[j]
+
+				var big, small int
+				if u > v {
+					big, small = u, v
+				} else {
+					big, small = v, u
+				}
+
+				valV := mat[i][v]
+
+				update := int((int64(valU) * int64(valV)) % MOD)
+				update = int((int64(update) * int64(inv)) % MOD)
+
+				old := mat[big][small]
+				mat[big][small] = (old - update + MOD) % MOD
+			}
+		}
+	}
 
 	fmt.Println(ans)
 }
