a6f1f5bb0bc228e09c8fff8718743315251e2897
dvaitam <137186606+dvaitam@users.noreply.github.com>
2025-11-22 17:34:24 +0530

    Fix 1776B verifier and reference solver

--- 1000-1999/1700-1799/1770-1779/1776/1776B.go
+++ 1000-1999/1700-1799/1770-1779/1776/1776B.go
@@ -8,22 +8,40 @@ import (
 
 func main() {
 	in := bufio.NewReader(os.Stdin)
+
 	var n int
 	var h int64
-	fmt.Fscan(in, &n, &h)
-	x := make([]int64, n)
+	if _, err := fmt.Fscan(in, &n, &h); err != nil {
+		return
+	}
+
+	a := make([]int64, n)
 	for i := 0; i < n; i++ {
-		fmt.Fscan(in, &x[i])
+		fmt.Fscan(in, &a[i])
 	}
-	var last int64 = -1 << 60
-	var cnt int64
-	for _, v := range x {
-		if last >= v-1 {
-			continue
+
+	f := make([][]int64, n)
+	for i := range f {
+		f[i] = make([]int64, n)
+	}
+
+	for l := n - 1; l >= 0; l-- {
+		for r := l + 1; r < n; r++ {
+			// Split the interval and take the best combination.
+			for k := l; k < r; k++ {
+				if f[l][k]+f[k+1][r] > f[l][r] {
+					f[l][r] = f[l][k] + f[k+1][r]
+				}
+			}
+
+			// Extend coverage if the two endpoints can be merged.
+			if a[l]+h >= a[r]-h {
+				diff := a[r] - a[l]
+				f[l][r] += h - (diff-1)/2
+			}
 		}
-		last = v + 1
-		cnt++
 	}
-	ans := cnt * h
+
+	ans := int64(n)*h - f[0][n-1]
 	fmt.Println(ans)
 }
