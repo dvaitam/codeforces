851a6e3e059e82629207001c3cc86dfff2fbdfa3
Anil <anil.xcode@gmail.com>
2025-11-28 09:30:06 +0000

    fix 13D, 13E and 14D

--- 0-999/0-99/10-19/13/13D.go
+++ 0-999/0-99/10-19/13/13D.go
@@ -1,101 +1,87 @@
 package main
 
 import (
-   "bufio"
-   "fmt"
-   "os"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 )
 
-type Point struct{ x, y int64 }
+var (
+	n, m int
+	A    [][]int
+	S    int
+
+	x, y []int64
+	X, Y []int64
+)
 
 func main() {
-   in := bufio.NewReader(os.Stdin)
-   var N, M int
-   if _, err := fmt.Fscan(in, &N, &M); err != nil {
-       return
-   }
-   reds := make([]Point, N)
-   for i := 0; i < N; i++ {
-       fmt.Fscan(in, &reds[i].x, &reds[i].y)
-   }
-   blues := make([]Point, M)
-   for i := 0; i < M; i++ {
-       fmt.Fscan(in, &blues[i].x, &blues[i].y)
-   }
-   if N < 3 {
-       fmt.Println(0)
-       return
-   }
-   // Special case: no blue points
-   if M == 0 {
-       // C(N,3)
-       n := int64(N)
-       fmt.Println(n * (n - 1) * (n - 2) / 6)
-       return
-   }
-   // bitsets: bs[i][j] bitset of blues to left of red[i]->red[j]
-   W := (M + 63) / 64
-   bs := make([][][]uint64, N)
-   for i := 0; i < N; i++ {
-       bs[i] = make([][]uint64, N)
-       for j := 0; j < N; j++ {
-           if i != j {
-               bs[i][j] = make([]uint64, W)
-           }
-       }
-   }
-   // fill bitsets
-   for b := 0; b < M; b++ {
-       bx, by := blues[b].x, blues[b].y
-       wi := b >> 6
-       mask := uint64(1) << (uint(b) & 63)
-       for i := 0; i < N; i++ {
-           xi, yi := reds[i].x, reds[i].y
-           for j := 0; j < N; j++ {
-               if i == j {
-                   continue
-               }
-               xj, yj := reds[j].x, reds[j].y
-               // cross of (j-i) and (b-i): >0 means left
-               if (xj-xi)*(by-yi) - (yj-yi)*(bx-xi) > 0 {
-                   bs[i][j][wi] |= mask
-               }
-           }
-       }
-   }
-   // count triangles without blues inside
-   var cnt int64
-   // iterate triples
-   for i := 0; i < N; i++ {
-       for j := i + 1; j < N; j++ {
-           xi, yi := reds[i].x, reds[i].y
-           xj, yj := reds[j].x, reds[j].y
-           for k := j + 1; k < N; k++ {
-               xk, yk := reds[k].x, reds[k].y
-               // orientation of i,j,k
-               cross := (xj-xi)*(yk-yi) - (yj-yi)*(xk-xi)
-               var a, b1, c int
-               if cross > 0 {
-                   a, b1, c = i, j, k
-               } else {
-                   a, b1, c = i, k, j
-               }
-               // check bitset intersection
-               ok := true
-               bsab := bs[a][b1]
-               bsc := bs[b1][c]
-               bsca := bs[c][a]
-               for w := 0; w < W; w++ {
-                   if bsab[w]&bsc[w]&bsca[w] != 0 {
-                       ok = false
-                       break
-                   }
-               }
-               if ok {
-                   cnt++
-               }
-           }
-       }
-   }
-   fmt.Println(cnt)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+
+	scanInt := func() int {
+		scanner.Scan()
+		val, _ := strconv.Atoi(scanner.Text())
+		return val
+	}
+	scanInt64 := func() int64 {
+		scanner.Scan()
+		val, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+		return val
+	}
+
+	if !scanner.Scan() {
+		return
+	}
+	n, _ = strconv.Atoi(scanner.Text())
+	m = scanInt()
+
+	x = make([]int64, n)
+	y = make([]int64, n)
+	for i := 0; i < n; i++ {
+		x[i] = scanInt64()
+		y[i] = scanInt64()
+	}
+
+	X = make([]int64, m)
+	Y = make([]int64, m)
+	for i := 0; i < m; i++ {
+		X[i] = scanInt64()
+		Y[i] = scanInt64()
+	}
+
+	A = make([][]int, n)
+	for i := range A {
+		A[i] = make([]int, n)
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if x[i] < x[j] {
+				a := y[j] - y[i]
+				b := x[i] - x[j]
+				c_val := -x[i]*a - y[i]*b
+
+				for k := 0; k < m; k++ {
+					if X[k] > x[i] && X[k] <= x[j] && a*X[k]+b*Y[k]+c_val > 0 {
+						A[i][j]++
+					}
+				}
+				A[j][i] = -A[i][j]
+			}
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			for k := j + 1; k < n; k++ {
+				if A[i][j]+A[j][k]+A[k][i] == 0 {
+					S++
+				}
+			}
+		}
+	}
+
+	fmt.Printf("%d\n", S)
 }
--- 0-999/0-99/10-19/13/13E.go
+++ 0-999/0-99/10-19/13/13E.go
@@ -1,77 +1,80 @@
 package main
 
 import (
-   "bufio"
-   "fmt"
-   "math"
-   "os"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 )
 
+const (
+	N = 100010
+	B = 350
+)
+
+var (
+	n, q int
+	a    [N]int
+	las  [N]int
+	cnt  [N]int
+)
+
+func get(i int) {
+	if i+a[i] >= n || i/B != (i+a[i])/B {
+		las[i] = i
+		cnt[i] = 0
+	} else {
+		las[i] = las[i+a[i]]
+		cnt[i] = cnt[i+a[i]] + 1
+	}
+}
+
 func main() {
-   reader := bufio.NewReader(os.Stdin)
-   var N, M int
-   if _, err := fmt.Fscan(reader, &N, &M); err != nil {
-       return
-   }
-   a := make([]int, N+2)
-   for i := 1; i <= N; i++ {
-       fmt.Fscan(reader, &a[i])
-   }
-   // Block size
-   B := int(math.Sqrt(float64(N))) + 1
-   blocks := (N + B - 1) / B
-   nxt := make([]int, N+2)
-   cnt := make([]int, N+2)
-   last := make([]int, N+2)
-   // rebuild block b
-   rebuild := func(b int) {
-       L := b*B + 1
-       R := (b + 1) * B
-       if R > N {
-           R = N
-       }
-       for i := R; i >= L; i-- {
-           j := i + a[i]
-           if j > N {
-               nxt[i] = N + 1
-               cnt[i] = 1
-               last[i] = i
-           } else if j >= L && j <= R {
-               nxt[i] = nxt[j]
-               cnt[i] = cnt[j] + 1
-               last[i] = last[j]
-           } else {
-               nxt[i] = j
-               cnt[i] = 1
-               last[i] = i
-           }
-       }
-   }
-   // initial build
-   for b := 0; b < blocks; b++ {
-       rebuild(b)
-   }
-   writer := bufio.NewWriter(os.Stdout)
-   defer writer.Flush()
-   for m := 0; m < M; m++ {
-       var typ int
-       fmt.Fscan(reader, &typ)
-       if typ == 0 {
-           var idx, val int
-           fmt.Fscan(reader, &idx, &val)
-           a[idx] = val
-           rebuild((idx - 1) / B)
-       } else {
-           var pos int
-           fmt.Fscan(reader, &pos)
-           total := 0
-           lastPos := pos
-           for pos <= N {
-               total += cnt[pos]
-               lastPos = last[pos]
-               pos = nxt[pos]
-           }
-           fmt.Fprintln(writer, lastPos, total)
-       }
-   }
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	scanInt := func() int {
+		if scanner.Scan() {
+			val, _ := strconv.Atoi(scanner.Text())
+			return val
+		}
+		return 0
+	}
+
+	n = scanInt()
+	q = scanInt()
+
+	for i := 0; i < n; i++ {
+		a[i] = scanInt()
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		get(i)
+	}
+
+	for ; q > 0; q-- {
+		o := scanInt()
+		if o == 1 {
+			p := scanInt()
+			p-- // 0-based index
+			dwn := 0
+			s := 0
+			for ; p < n; {
+				dwn = las[p]
+				s += cnt[p] + 1
+				p = dwn + a[dwn]
+			}
+			fmt.Fprintf(writer, "%d %d\n", dwn+1, s)
+		} else {
+			p := scanInt()
+			v := scanInt()
+			p-- // 0-based index
+			a[p] = v
+			for i := p; i >= 0 && i/B == p/B; i-- {
+				get(i)
+			}
+		}
+	}
 }
