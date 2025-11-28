75b4d359715a0e8985207ea319baf698c9f08df5
Anil <anil.xcode@gmail.com>
2025-11-28 06:03:49 +0000

    update verifier and sol

--- 0-999/0-99/0-9/3/3D.go
+++ 0-999/0-99/0-9/3/3D.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-   "bufio"
-   "container/heap"
-   "fmt"
-   "os"
+	"bufio"
+	"container/heap"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 // item holds the cost difference and position for a '?' placeholder
 type item struct {
-   diff int // cost to change from '(' to ')'
-   idx  int // position in result
+	diff int // cost change from ')' to '(' (A - B)
+	idx  int // position in result
 }
 
 // minHeap implements a min-heap of items based on diff
@@ -21,88 +22,88 @@ func (h minHeap) Less(i, j int) bool  { return h[i].diff < h[j].diff }
 func (h minHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
 func (h *minHeap) Push(x interface{}) { *h = append(*h, x.(item)) }
 func (h *minHeap) Pop() interface{} {
-   old := *h
-   n := len(old)
-   it := old[n-1]
-   *h = old[:n-1]
-   return it
+	old := *h
+	n := len(old)
+	it := old[n-1]
+	*h = old[:n-1]
+	return it
 }
 
 func main() {
-   reader := bufio.NewReader(os.Stdin)
-   // read pattern
-   var pattern string
-   if _, err := fmt.Fscanln(reader, &pattern); err != nil {
-       fmt.Println(-1)
-       return
-   }
-   // count '?' to know how many cost pairs
-   qCount := 0
-   for _, ch := range pattern {
-       if ch == '?' {
-           qCount++
-       }
-   }
-   // read cost pairs
-   costs := make([][2]int, qCount)
-   for i := 0; i < qCount; i++ {
-       if _, err := fmt.Fscan(reader, &costs[i][0], &costs[i][1]); err != nil {
-           fmt.Println(-1)
-           return
-       }
-   }
-   // prepare result as runes
-   res := []rune(pattern)
-   h := &minHeap{}
-   heap.Init(h)
-   balance := 0
-   totalCost := 0
-   qi := 0
-   // initial assignment: treat all '?' as '(' and push diff
-   for i, ch := range res {
-       switch ch {
-       case '(':
-           balance++
-       case ')':
-           balance--
-       case '?':
-           a := costs[qi][0]
-           b := costs[qi][1]
-           totalCost += a
-           res[i] = '('
-           // diff is extra cost to flip to ')'
-           heap.Push(h, item{diff: b - a, idx: i})
-           balance++
-           qi++
-       }
-       if balance < 0 {
-           // need to flip a previous '?' to ')'
-           if h.Len() == 0 {
-               fmt.Println(-1)
-               return
-           }
-           it := heap.Pop(h).(item)
-           res[it.idx] = ')'
-           totalCost += it.diff
-           balance += 2
-       }
-   }
-   // close extra '(' by flipping
-   for balance > 0 {
-       if h.Len() == 0 {
-           fmt.Println(-1)
-           return
-       }
-       it := heap.Pop(h).(item)
-       res[it.idx] = ')'
-       totalCost += it.diff
-       balance -= 2
-   }
-   if balance != 0 {
-       fmt.Println(-1)
-       return
-   }
-   // output result
-   fmt.Println(totalCost)
-   fmt.Println(string(res))
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+
+	if !scanner.Scan() {
+		return
+	}
+	pattern := scanner.Text()
+
+	n := len(pattern)
+	if n%2 != 0 {
+		fmt.Println("-1")
+		return
+	}
+
+	res := []byte(pattern)
+	h := &minHeap{}
+	heap.Init(h)
+
+	var totalCost int64
+	balance := 0
+	
+	// We need to read pairs of costs for each '?'
+	// Iterate through the pattern and process '?' as we go
+	
+	for i := 0; i < n; i++ {
+		ch := res[i]
+		if ch == '(' {
+			balance++
+		} else if ch == ')' {
+			balance--
+		} else { // ch == '?'
+			// Read costs for this '?'
+			if !scanner.Scan() {
+				break // Should not happen based on valid input
+			}
+			aStr := scanner.Text()
+			if !scanner.Scan() {
+				break
+			}
+			bStr := scanner.Text()
+
+			a, _ := strconv.Atoi(aStr)
+			b, _ := strconv.Atoi(bStr)
+
+			// Greedy strategy: Assume ')' first
+			res[i] = ')'
+			balance--
+			totalCost += int64(b)
+
+			// Push option to swap to '('
+			// Cost change: new_cost - old_cost = a - b
+			heap.Push(h, item{diff: a - b, idx: i})
+		}
+
+		// If balance is negative, we MUST flip a previous ')' to '('
+		if balance < 0 {
+			if h.Len() == 0 {
+				fmt.Println("-1")
+				return
+			}
+			it := heap.Pop(h).(item)
+			
+			// Apply flip
+			res[it.idx] = '('
+			totalCost += int64(it.diff)
+			balance += 2 // ')' (-1) -> '(' (+1) is a change of +2
+		}
+	}
+
+	if balance != 0 {
+		fmt.Println("-1")
+	} else {
+		fmt.Println(totalCost)
+		fmt.Println(string(res))
+	}
 }
+
