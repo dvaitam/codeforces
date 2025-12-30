63bdedd7f482909a22371acd852a1420161b5141
Anil <anil.xcode@gmail.com>
2025-12-30 07:17:50 +0000

    update

--- 0-999/200-299/200-209/207/207C3.go
+++ 0-999/200-299/200-209/207/207C3.go
@@ -6,277 +6,238 @@ import (
 	"os"
 )
 
-type lctPath struct {
-	ch  [2][]int
-	fa  []int
-	val []int64
-	sum []int64
+const MAXN = 100005
+const LOGN = 18
+const BASE = 131
+
+var (
+	reader *bufio.Reader
+	writer *bufio.Writer
+)
+
+type Edge struct {
+	to   int
+	char byte
 }
 
-func newLCTPath(n int) *lctPath {
-	l := &lctPath{
-		fa:  make([]int, n),
-		val: make([]int64, n),
-		sum: make([]int64, n),
-	}
-	l.ch[0] = make([]int, n)
-	l.ch[1] = make([]int, n)
-	return l
-}
+var (
+	adj1    = make([][]Edge, MAXN)
+	adj2    = make([][]Edge, MAXN)
+	ops     = make([]Operation, MAXN)
+	map1    = make([]int, MAXN) // T1 node -> Trie node
+	map2    = make([]int, MAXN) // T2 node -> Trie node
 
-func (l *lctPath) isRoot(x int) bool {
-	f := l.fa[x]
-	return f == 0 || (l.ch[0][f] != x && l.ch[1][f] != x)
-}
+	// Trie
+	trieNext = make([][26]int, MAXN)
+	trieCnt  = 1
+	// trieAdj is implicit via structure, but we need DFS order
+	// so we can use trieNext for that.
 
-func (l *lctPath) pushUp(x int) {
-	if x == 0 {
-		return
-	}
-	l.sum[x] = l.val[x] + l.sum[l.ch[0][x]] + l.sum[l.ch[1][x]]
-}
+	// Hashes and LCA
+	powB      = make([]uint64, MAXN)
+	trieHash  = make([]uint64, MAXN)
+	hashMap   = make(map[uint64]int)
+	
+	up2      = make([][LOGN]int, MAXN)
+	upHash2  = make([]uint64, MAXN)
+	depth2   = make([]int, MAXN)
+	
+	// BITs
+	bit1     = make([]int64, MAXN) // Subtree Sum K
+	bit2     = make([]int64, MAXN) // Path Sum W (Range Update Point Query)
+	tin      = make([]int, MAXN)
+	tout     = make([]int, MAXN)
+	timer    int
+)
 
-func (l *lctPath) rotate(x int) {
-	y := l.fa[x]
-	z := l.fa[y]
-	dir := 0
-	if l.ch[1][y] == x {
-		dir = 1
-	}
-	b := l.ch[dir^1][x]
-	if !l.isRoot(y) {
-		if l.ch[0][z] == y {
-			l.ch[0][z] = x
-		} else if l.ch[1][z] == y {
-			l.ch[1][z] = x
-		}
-	}
-	l.fa[x] = z
-	l.ch[dir^1][x] = y
-	l.fa[y] = x
-	l.ch[dir][y] = b
-	if b != 0 {
-		l.fa[b] = y
-	}
-	l.pushUp(y)
-	l.pushUp(x)
-}
-
-func (l *lctPath) splay(x int) {
-	for !l.isRoot(x) {
-		y := l.fa[x]
-		z := l.fa[y]
-		if !l.isRoot(y) {
-			if (l.ch[0][y] == x) == (l.ch[0][z] == y) {
-				l.rotate(y)
-			} else {
-				l.rotate(x)
-			}
-		}
-		l.rotate(x)
-	}
-}
-
-func (l *lctPath) access(x int) {
-	last := 0
-	for v := x; v != 0; v = l.fa[v] {
-		l.splay(v)
-		l.ch[1][v] = last
-		l.pushUp(v)
-		last = v
-	}
-	l.splay(x)
-}
-
-func (l *lctPath) addValue(x int, delta int64) {
-	l.access(x)
-	l.val[x] += delta
-	l.pushUp(x)
-}
-
-func (l *lctPath) queryPathSum(x int) int64 {
-	l.access(x)
-	return l.sum[x]
-}
-
-type lctSub struct {
-	ch   [2][]int
-	fa   []int
-	val  []int64
-	sum  []int64
-	vsum []int64
-}
-
-func newLCTSub(n int) *lctSub {
-	l := &lctSub{
-		fa:   make([]int, n),
-		val:  make([]int64, n),
-		sum:  make([]int64, n),
-		vsum: make([]int64, n),
-	}
-	l.ch[0] = make([]int, n)
-	l.ch[1] = make([]int, n)
-	return l
-}
-
-func (l *lctSub) isRoot(x int) bool {
-	f := l.fa[x]
-	return f == 0 || (l.ch[0][f] != x && l.ch[1][f] != x)
-}
-
-func (l *lctSub) pushUp(x int) {
-	if x == 0 {
-		return
-	}
-	l.sum[x] = l.val[x] + l.sum[l.ch[0][x]] + l.sum[l.ch[1][x]] + l.vsum[x]
-}
-
-func (l *lctSub) rotate(x int) {
-	y := l.fa[x]
-	z := l.fa[y]
-	dir := 0
-	if l.ch[1][y] == x {
-		dir = 1
-	}
-	b := l.ch[dir^1][x]
-	if !l.isRoot(y) {
-		if l.ch[0][z] == y {
-			l.ch[0][z] = x
-		} else if l.ch[1][z] == y {
-			l.ch[1][z] = x
-		}
-	}
-	l.fa[x] = z
-	l.ch[dir^1][x] = y
-	l.fa[y] = x
-	l.ch[dir][y] = b
-	if b != 0 {
-		l.fa[b] = y
-	}
-	l.pushUp(y)
-	l.pushUp(x)
-}
-
-func (l *lctSub) splay(x int) {
-	for !l.isRoot(x) {
-		y := l.fa[x]
-		z := l.fa[y]
-		if !l.isRoot(y) {
-			if (l.ch[0][y] == x) == (l.ch[0][z] == y) {
-				l.rotate(y)
-			} else {
-				l.rotate(x)
-			}
-		}
-		l.rotate(x)
-	}
-}
-
-func (l *lctSub) access(x int) {
-	last := 0
-	for v := x; v != 0; v = l.fa[v] {
-		l.splay(v)
-		l.vsum[v] += l.sum[l.ch[1][v]]
-		l.ch[1][v] = last
-		l.vsum[v] -= l.sum[last]
-		l.pushUp(v)
-		last = v
-	}
-	l.splay(x)
-}
-
-func (l *lctSub) addValue(x int, delta int64) {
-	l.access(x)
-	l.val[x] += delta
-	l.pushUp(x)
-}
-
-func (l *lctSub) querySubtree(x int) int64 {
-	l.access(x)
-	return l.val[x] + l.vsum[x]
-}
-
-func (l *lctSub) link(parent, child int) {
-	l.fa[child] = parent
-	l.vsum[parent] += l.sum[child]
-	l.pushUp(parent)
+type Operation struct {
+	t, v int
+	c    byte
 }
 
 func main() {
-	in := bufio.NewReader(os.Stdin)
-	var n int
-	if _, err := fmt.Fscan(in, &n); err != nil {
-		return
-	}
+	reader = bufio.NewReader(os.Stdin)
+	writer = bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
 
-	maxNodes := n + 5
-	children := make([][26]int, maxNodes)
-	fail := make([]int, maxNodes)
+	var numOps int
+	fmt.Fscan(reader, &numOps)
+	
+	cnt1 := 1
+	cnt2 := 1
 
-	lctW := newLCTPath(maxNodes)
-	lctF := newLCTSub(maxNodes)
-
-	state1 := make([]int, n+2)
-	state2 := make([]int, n+2)
-	verts1, verts2 := 1, 1
-	root := 1
-	state1[1] = root
-	state2[1] = root
-	totalStates := root
-
-	// root pattern (empty string) exists in tree1 and tree2 initially
-	lctW.addValue(root, 1)
-	lctF.addValue(root, 1)
-	ans := lctW.queryPathSum(root)
-
-	out := bufio.NewWriter(os.Stdout)
-	defer out.Flush()
-
-	for i := 0; i < n; i++ {
+	for i := 0; i < numOps; i++ {
 		var t, v int
-		var c string
-		fmt.Fscan(in, &t, &v, &c)
-		ch := int(c[0] - 'a')
+		var s string
+		fmt.Fscan(reader, &t, &v, &s)
+		c := s[0]
+		ops[i] = Operation{t, v, c}
 		if t == 1 {
-			parentState := state1[v]
-			child := children[parentState][ch]
-			if child == 0 {
-				totalStates++
-				child = totalStates
-				children[parentState][ch] = child
-				f := fail[parentState]
-				for f != 0 && children[f][ch] == 0 {
-					f = fail[f]
-				}
-				if children[f][ch] != 0 {
-					f = children[f][ch]
-				} else {
-					f = root
-				}
-				fail[child] = f
-				lctW.fa[child] = f
-				lctF.link(f, child)
-			}
-			verts1++
-			state1[verts1] = child
-			lctW.addValue(child, 1)
-			ans += lctF.querySubtree(child)
+			cnt1++
+			adj1[v] = append(adj1[v], Edge{cnt1, c})
 		} else {
-			parentState := state2[v]
-			s := parentState
-			for s != 0 && children[s][ch] == 0 {
-				s = fail[s]
-			}
-			if children[s][ch] != 0 {
-				s = children[s][ch]
-			} else {
-				s = root
-			}
-			verts2++
-			state2[verts2] = s
-			lctF.addValue(s, 1)
-			ans += lctW.queryPathSum(s)
+			cnt2++
+			adj2[v] = append(adj2[v], Edge{cnt2, c})
 		}
-		fmt.Fprintln(out, ans)
 	}
+
+	// Build Trie from T1
+	buildTrie(1, 1) // T1 root is 1, Trie root is 1
+	
+	// Precompute powers
+	powB[0] = 1
+	for i := 1; i < MAXN; i++ {
+		powB[i] = powB[i-1] * BASE
+	}
+
+	// Trie DFS for hashing and BIT range
+	dfsTrie(1, 0)
+
+	// T2 processing
+	dfsT2(1, 0, 0)
+	
+	// Precompute map2 for all T2 nodes
+	for i := 1; i <= cnt2; i++ {
+		map2[i] = binarySearchMatch(i)
+	}
+
+	// Initial state setup
+	ans := int64(1)
+	updateBIT2(tin[1], tout[1], 1) // Activate root 1 of T1
+	updateBIT1(tin[1], 1)          // Activate root 1 of T2
+
+	currM1 := 1
+	currM2 := 1
+	
+	for i := 0; i < numOps; i++ {
+		op := ops[i]
+		if op.t == 1 {
+			currM1++
+			u := currM1
+			tNode := map1[u]
+			// Activate u in T1
+			// Update BIT2: add 1 to subtree of tNode in Trie (effectively path sum increase)
+			updateBIT2(tin[tNode], tout[tNode], 1)
+			// Query BIT1: sum K in subtree of tNode
+			term := queryBIT1(tout[tNode]) - queryBIT1(tin[tNode]-1)
+			ans += term
+		} else {
+			currM2++
+			v := currM2
+			tNode := map2[v]
+			// Activate v in T2
+			// Update BIT1: add 1 to point tin[tNode]
+			updateBIT1(tin[tNode], 1)
+			// Query BIT2: value at tin[tNode]
+			term := queryBIT2(tin[tNode])
+			ans += term
+		}
+		fmt.Fprintln(writer, ans)
+	}
+}
+
+// Build Trie from T1 by DFS
+func buildTrie(u int, trieNode int) {
+	map1[u] = trieNode
+	for _, e := range adj1[u] {
+		cIdx := int(e.char - 'a')
+		if trieNext[trieNode][cIdx] == 0 {
+			trieCnt++
+			trieNext[trieNode][cIdx] = trieCnt
+		}
+		buildTrie(e.to, trieNext[trieNode][cIdx])
+	}
+}
+
+func dfsTrie(u int, d int) {
+	timer++
+	tin[u] = timer
+	hashMap[trieHash[u]] = u
+	
+	for c := 0; c < 26; c++ {
+		v := trieNext[u][c]
+		if v != 0 {
+			trieHash[v] = trieHash[u] + uint64(c+'a')*powB[d]
+			dfsTrie(v, d+1)
+		}
+	}
+	tout[u] = timer
+}
+
+func dfsT2(u int, p int, d int) {
+	depth2[u] = d
+	up2[u][0] = p
+	for j := 1; j < LOGN; j++ {
+		up2[u][j] = up2[up2[u][j-1]][j-1]
+	}
+	for _, e := range adj2[u] {
+		upHash2[e.to] = uint64(e.char) + upHash2[u] * BASE
+		dfsT2(e.to, u, d+1)
+	}
+}
+
+func binarySearchMatch(v int) int {
+	low, high := 0, depth2[v]
+	bestNode := 1 
+	
+	for low <= high {
+		mid := (low + high) / 2
+		if mid == 0 {
+			low = 1
+			continue
+		}
+		
+		ancestor := getKthAncestor(v, mid)
+		currentH := upHash2[v] - upHash2[ancestor] * powB[mid]
+		
+		if node, ok := hashMap[currentH]; ok {
+			bestNode = node
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return bestNode
+}
+
+func getKthAncestor(node, k int) int {
+	for i := 0; i < LOGN; i++ {
+		if (k>>i)&1 == 1 {
+			node = up2[node][i]
+		}
+	}
+	return node
+}
+
+func updateBIT1(idx int, val int64) {
+	for ; idx <= trieCnt; idx += idx & -idx {
+		bit1[idx] += val
+	}
+}
+
+func queryBIT1(idx int) int64 {
+	sum := int64(0)
+	for ; idx > 0; idx -= idx & -idx {
+		sum += bit1[idx]
+	}
+	return sum
+}
+
+func updateBIT2(l, r int, val int64) {
+	internalUpdateBIT2(l, val)
+	internalUpdateBIT2(r+1, -val)
+}
+
+func internalUpdateBIT2(idx int, val int64) {
+	for ; idx <= trieCnt; idx += idx & -idx {
+		bit2[idx] += val
+	}
+}
+
+func queryBIT2(idx int) int64 {
+	sum := int64(0)
+	for ; idx > 0; idx -= idx & -idx {
+		sum += bit2[idx]
+	}
+	return sum
 }
--- 1000-1999/1000-1099/1020-1029/1025/solE.cpp
+++ 1000-1999/1000-1099/1020-1029/1025/solE.cpp
@@ -1,179 +1,68 @@
-//#pragma comment(linker, "/STACK:134217728")
-
-#include <iostream>
-#include <sstream>
-#include <cstdio>
-#include <cstdlib>
-#include <cmath>
-#include <memory>
-#include <cctype>
-#include <cstring>
-#include <vector>
-#include <list>
-#include <queue>
-#include <deque>
-#include <stack>
-#include <map>
-#include <set>
-#include <algorithm>
-#include <numeric>
-using namespace std;
-
-typedef long long Int;
-typedef pair<int,int> PII;
-typedef vector<int> VInt;
-
-#define FOR(i, a, b) for(i = (a); i < (b); ++i)
-#define RFOR(i, a, b) for(i = (a) - 1; i >= (b); --i)
-#define CLEAR(a, b) memset(a, b, sizeof(a))
-#define SIZE(a) int((a).size())
-#define ALL(a) (a).begin(),(a).end()
-#define PB push_back
-#define MP make_pair
-
-const int DR[] = { -1, 1, 0, 0 };
-const int DC[] = { 0, 0, -1, 1 };
-
-int R[64];
-int C[64];
-int TR[64];
-int TC[64];
-int A[64][64];
-vector<pair<PII, PII>> res;
-
-void move(int id, int r, int c)
-{
-	if (R[id] != r && C[id] != c) throw 0;
-	int dir = -1;
-	if (R[id] > r) dir = 0;
-	if (R[id] < r) dir = 1;
-	if (C[id] > c) dir = 2;
-	if (C[id] < c) dir = 3;
-	if (dir == -1) return;
-
-	while(R[id] != r || C[id] != c)
-	{
-		PII p1(R[id], C[id]);
-		if (A[R[id]][C[id]] != id) throw 0;
-		A[R[id]][C[id]] = -1;
-		R[id] += DR[dir];
-		C[id] += DC[dir];
-		if (A[R[id]][C[id]] != -1) throw 0;
-		A[R[id]][C[id]] = id;
-		PII p2(R[id], C[id]);
-		res.push_back(MP(p1, p2));
+#include<cstdio>
+const int vx[4]={-1,0,0,1},vy[4]={0,-1,1,0};
+/*
+ 0
+1 2
+ 3
+*/
+int n,m;
+struct Map{
+	int a[55][55],k,x1[10800],y1[10800],x2[10800],y2[10800];
+	void read(){
+		for(int i=1,x,y;i<=m;i++)scanf("%d%d",&x,&y),a[x][y]=i;
 	}
-}
-
-int solve(int n, int m)
-{
-	res.clear();
-	if (n == 1) return 0;
-	int i;
-	vector<pair<PII, int>> v(m);
-	FOR(i, 0, m)
-		v[i] = MP(PII(R[i], R[i] == 0 ? C[i] : -C[i]), i);
-
-	sort(ALL(v));
-	FOR(i, 0, m)
-	{
-		int tr = 0;
-		int tc = i;
-		int id = v[i].second;
-
-		if (C[id] < tc)	move(id, R[id], tc);
-		move(id, tr, C[id]);
-		move(id, tr, tc);
+	void move(int x, int y, int d){
+		a[x+vx[d]][y+vy[d]]=a[x][y];
+		a[x][y]=0;
+		x1[k]=x;
+		y1[k]=y;
+		x2[k]=x+vx[d];
+		y2[k++]=y+vy[d];
 	}
-
-	FOR(i, 0, m)
-		v[i] = MP(PII(TR[i], TC[i]), i);
-
-	sort(ALL(v));
-	RFOR(i, m, 0)
-		if (v[i].first.first > 1)
-		{
-			int id = v[i].second;
-			move(id, 1, C[id]);
-			move(id, R[id], TC[id]);
-			move(id, TR[id], TC[id]);
-		}
-
-	v.clear();
-	FOR(i, 0, m)
-		if (TR[i] <= 1)
-			v.push_back(MP(PII(TR[i], TR[i] == 0 ? TC[i] : -TC[i]), i));
-
-
-	sort(ALL(v));
-	int am = SIZE(v);
-	FOR(i, 0, am)
-	{
-		int id = v[i].second;
-		if (C[id] < i) throw 0;
-		while (C[id] != i)
-		{
-			if (A[0][C[id] - 1] == -1) move(id, 0, C[id] - 1);
-			else
-			{
-				int id2 = A[0][C[id] - 1];
-				move(id2, 1, C[id2]);
-				move(id, 0, C[id] - 1);
-				move(id2, 1, C[id2] + 1);
-				move(id2, 0, C[id2]);
+	void move_r(int x, int y){
+		if(y+1<=n&&a[x][y+1])move_r(x,y+1);
+		move(x,y,2);
+	}
+	void play(){
+		int pos=0;
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=n;j++)if(a[i][j]){
+				int x=j;++pos;
+				while(x<pos)move_r(i,x++);
+				while(x>pos)move(i,x--,1);
+				for(x=i;x>1;x--)move(x,pos,0);
 			}
 		}
 	}
-
-	RFOR(i, am, 0)
-	{
-		int id = v[i].second;
-		if (TR[id] != 0)
-		{
-			move(id, 0, n - 1);
-			move(id, 1, n - 1);
+	void fix(int*b){
+		if(n==2&&a[1][1]!=b[1]){
+			move(1,1,3);move(2,1,2);
+			move(1,2,1);move(2,2,0);
 		}
-		move(id, TR[id], TC[id]);
+		else if(n>3){
+			for(int i=1;i<=m;i++){
+				int j=1,x=i;while(b[j]!=a[1][i])j++;
+				move(1,i,3);
+				while(x<j)move(2,x++,2);
+				while(x>j)move(2,x--,1);
+				move(2,x,3);
+			}
+			for(int i=1;i<=m;i++)move(3,i,0),move(2,i,0);
+		}
 	}
-
-	return SIZE(res);
+	void print(bool r){
+		if(r)for(int i=k;i--;)printf("%d %d %d %d\n",x2[i],y2[i],x1[i],y1[i]);
+		else for(int i=0;i<k;i++)printf("%d %d %d %d\n",x1[i],y1[i],x2[i],y2[i]);
+	}
+}M1,M2;
+int main(){
+	scanf("%d%d",&n,&m);
+	M1.read();
+	M2.read();
+	M1.play();
+	M2.play();
+	M1.fix(M2.a[1]);
+	printf("%d\n",M1.k+M2.k);
+	M1.print(0);
+	M2.print(1);
 }
-
-int main()
-{
-	int n, m;
-	n = m = 4;
-
-	int i;
-
-	scanf("%d%d", &n, &m);
-	CLEAR(A, -1);
-	FOR(i, 0, m)
-	{
-		int r, c;
-		scanf("%d%d", &r, &c);
-		--r;
-		--c;
-		R[i] = r;
-		C[i] = c;
-		A[r][c] = i;
-	}
-	FOR(i, 0, m)
-	{
-		int r, c;
-		scanf("%d%d", &r, &c);
-		--r;
-		--c;
-		TR[i] = r;
-		TC[i] = c;
-	}
-
-	solve(n, m);
-
-
-	printf("%d\n", SIZE(res));
-	FOR(i, 0, SIZE(res))
-		printf("%d %d %d %d\n", res[i].first.first + 1, res[i].first.second + 1, res[i].second.first + 1, res[i].second.second + 1);
-
-	return 0;
-};
