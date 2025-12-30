533db112f3e1e1e22fedc2d084b829f9d4249d46
Anil <anil.xcode@gmail.com>
2025-12-30 07:23:02 +0000

    update contest 1009

--- 1000-1999/1000-1099/1000-1009/1009/1009F.go
+++ 1000-1999/1000-1099/1000-1009/1009/1009F.go
@@ -1,111 +1,89 @@
 package main
 
 import (
-   "bufio"
-   "fmt"
-   "os"
+	"bufio"
+	"fmt"
+	"os"
 )
+
+const N = 1000010
 
 var (
-   n       int
-   reader  *bufio.Reader
-   writer  *bufio.Writer
-   last    []int
-   eNext   []int
-   eTo     []int
-   ecnt    int
-   mem     []int32
-   pos     []int
-   ln      []int
-   mx      []int32
-   mxid    []int
-   ans     []int
+	adj   [N][]int
+	t     [N][]int
+	mx    [N]int
+	ans   [N]int
+	n     int
 )
 
-func in() int {
-   var x int
-   c, err := reader.ReadByte()
-   for err == nil && (c < '0' || c > '9') {
-       c, err = reader.ReadByte()
-   }
-   for err == nil && c >= '0' && c <= '9' {
-       x = x*10 + int(c-'0')
-       c, err = reader.ReadByte()
-   }
-   return x
+// merge merges two slices using the smaller-to-larger trick.
+// p and q represent the indices of the maximum values in slices x and y.
+func merge(u, v int) {
+	// If the child's slice is larger, swap it with the parent's slice.
+	// This is equivalent to x.swap(y) in C++.
+	if len(t[u]) < len(t[v]) {
+		t[u], t[v] = t[v], t[u]
+		mx[u], mx[v] = mx[v], mx[u]
+	}
+
+	// Merge the smaller slice into the larger one.
+	// Aligns the ends of the slices (depth-based).
+	du := len(t[u])
+	dv := len(t[v])
+	diff := du - dv
+
+	for i := 0; i < dv; i++ {
+		targetIdx := i + diff
+		t[u][targetIdx] += t[v][i]
+
+		// Update the index of the maximum frequency (mx[u]).
+		// Condition: new frequency is strictly greater, 
+		// or equal but at a shallower depth (smaller index relative to the end).
+		if t[u][targetIdx] > t[u][mx[u]] || (t[u][targetIdx] == t[u][mx[u]] && targetIdx > mx[u]) {
+			mx[u] = targetIdx
+		}
+	}
+	// Free memory for the merged-away slice
+	t[v] = nil 
 }
 
-func addEdge(a, b int) {
-   ecnt++
-   eNext[ecnt] = last[a]
-   eTo[ecnt] = b
-   last[a] = ecnt
-}
+func dfs(u, fa int) {
+	for _, v := range adj[u] {
+		if v != fa {
+			dfs(v, u)
+			merge(u, v)
+		}
+	}
 
-func merge(x, y int) {
-   if ln[x] < ln[y] {
-       pos[x], pos[y] = pos[y], pos[x]
-       ln[x], ln[y] = ln[y], ln[x]
-       mx[x], mx[y] = mx[y], mx[x]
-       mxid[x], mxid[y] = mxid[y], mxid[x]
-   }
-   baseX := pos[x]
-   baseY := pos[y]
-   for i := 0; i < ln[y]; i++ {
-       mem[baseX+i] += mem[baseY+i]
-       if mem[baseX+i] > mx[x] {
-           mx[x] = mem[baseX+i]
-           mxid[x] = i
-       } else if mem[baseX+i] == mx[x] && i < mxid[x] {
-           mxid[x] = i
-       }
-   }
-}
-
-func dfs(x, fa int) {
-   mx[x] = 1
-   mxid[x] = 0
-   ln[x] = 1
-   mem[pos[x]] = 1
-   for i := last[x]; i != 0; i = eNext[i] {
-       y := eTo[i]
-       if y == fa {
-           continue
-       }
-       pos[y] = pos[x] + ln[x]
-       dfs(y, x)
-       ln[y]++
-       mem[pos[y]] = 0
-       mxid[y]++
-       merge(x, y)
-   }
-   ans[x] = mxid[x]
+	// Add the current node itself (depth 0 relative to itself)
+	t[u] = append(t[u], 1)
+	
+	// If the current node (count 1) is the new maximum frequency
+	if t[u][mx[u]] == 1 {
+		mx[u] = len(t[u]) - 1
+	}
+	
+	// The answer is the depth (distance from u), which is (TotalLength - 1) - mxIdx
+	ans[u] = len(t[u]) - 1 - mx[u]
 }
 
 func main() {
-   reader = bufio.NewReader(os.Stdin)
-   writer = bufio.NewWriter(os.Stdout)
-   defer writer.Flush()
-   n = in()
-   // allocate based on n
-   last = make([]int, n+2)
-   eNext = make([]int, (n*2)+2)
-   eTo = make([]int, (n*2)+2)
-   mem = make([]int32, (n*23)+2)
-   pos = make([]int, n+2)
-   ln = make([]int, n+2)
-   mx = make([]int32, n+2)
-   mxid = make([]int, n+2)
-   ans = make([]int, n+2)
-   for i := 1; i < n; i++ {
-       a := in()
-       b := in()
-       addEdge(a, b)
-       addEdge(b, a)
-   }
-   pos[1] = 0
-   dfs(1, 0)
-   for i := 1; i <= n; i++ {
-       fmt.Fprintln(writer, ans[i])
-   }
+	// Use fast I/O for 10^6 nodes
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	fmt.Fscan(reader, &n)
+	for i := 1; i < n; i++ {
+		var u, v int
+		fmt.Fscan(reader, &u, &v)
+		adj[u] = append(adj[u], v)
+		adj[v] = append(adj[v], u)
+	}
+
+	dfs(1, 0)
+
+	for i := 1; i <= n; i++ {
+		fmt.Fprintln(writer, ans[i])
+	}
 }
