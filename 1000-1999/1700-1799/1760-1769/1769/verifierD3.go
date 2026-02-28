ac2670b98c3fefecf9fff088307c1b66d1ee01cf
dvaitam <137186606+dvaitam@users.noreply.github.com>
2026-02-28 18:14:19 +0530

    Optimize 1769 D3 verifier game search (#6937)

--- 1000-1999/1700-1799/1760-1769/1769/verifierD3.go
+++ 1000-1999/1700-1799/1760-1769/1769/verifierD3.go
@@ -10,6 +10,95 @@ import (
 )
 
 const totalCards = 36
+const (
+	suitStates  = 25
+	totalStates = suitStates * suitStates * suitStates * suitStates
+)
+
+type stateMove struct {
+	bit  uint64
+	next int
+}
+
+var (
+	stateMasks     [totalStates]uint64
+	stateMoveCount [totalStates]uint8
+	stateMoves     [totalStates][8]stateMove
+)
+
+func suitCode(l, r int) int {
+	// 0 means empty; other states are intervals [l, r] containing rank 9 (index 3).
+	if l == -1 {
+		return 0
+	}
+	return 1 + l*6 + (r - 3)
+}
+
+func decodeSuit(code int) (int, int) {
+	if code == 0 {
+		return -1, -1
+	}
+	code--
+	l := code / 6
+	r := 3 + code%6
+	return l, r
+}
+
+func initStateGraph() {
+	mul := [4]int{suitStates * suitStates * suitStates, suitStates * suitStates, suitStates, 1}
+	for s0 := 0; s0 < suitStates; s0++ {
+		l0, r0 := decodeSuit(s0)
+		for s1 := 0; s1 < suitStates; s1++ {
+			l1, r1 := decodeSuit(s1)
+			for s2 := 0; s2 < suitStates; s2++ {
+				l2, r2 := decodeSuit(s2)
+				for s3 := 0; s3 < suitStates; s3++ {
+					l3, r3 := decodeSuit(s3)
+					s := [4]int{s0, s1, s2, s3}
+					ls := [4]int{l0, l1, l2, l3}
+					rs := [4]int{r0, r1, r2, r3}
+					idx := s0*mul[0] + s1*mul[1] + s2*mul[2] + s3
+
+					var played uint64
+					var cnt uint8
+					for suit := 0; suit < 4; suit++ {
+						l, r := ls[suit], rs[suit]
+						if l == -1 {
+							bit := uint64(1) << (suit*9 + 3)
+							next := idx + (suitCode(3, 3)-s[suit])*mul[suit]
+							stateMoves[idx][cnt] = stateMove{bit: bit, next: next}
+							cnt++
+							continue
+						}
+						for rank := l; rank <= r; rank++ {
+							played |= uint64(1) << (suit*9 + rank)
+						}
+						if l > 0 {
+							rank := l - 1
+							bit := uint64(1) << (suit*9 + rank)
+							next := idx + (suitCode(l-1, r)-s[suit])*mul[suit]
+							stateMoves[idx][cnt] = stateMove{bit: bit, next: next}
+							cnt++
+						}
+						if r < 8 {
+							rank := r + 1
+							bit := uint64(1) << (suit*9 + rank)
+							next := idx + (suitCode(l, r+1)-s[suit])*mul[suit]
+							stateMoves[idx][cnt] = stateMove{bit: bit, next: next}
+							cnt++
+						}
+					}
+					stateMasks[idx] = played
+					stateMoveCount[idx] = cnt
+				}
+			}
+		}
+	}
+}
+
+func init() {
+	initStateGraph()
+}
 
 func cardIndex(card string) (int, bool) {
 	if len(card) != 2 {
@@ -28,51 +117,36 @@ func cardIndex(card string) (int, bool) {
 type gameSolver struct {
 	aMask uint64
 	bMask uint64
-	memo  map[uint64]int
+	memo  [2][totalStates]int
 }
 
-func (g *gameSolver) legalMoves(rem, played uint64) []uint64 {
-	moves := make([]uint64, 0, bits.OnesCount64(rem))
-	for i := 0; i < totalCards; i++ {
-		bit := uint64(1) << i
-		if rem&bit == 0 {
-			continue
-		}
-		r := i % 9
-		ok := false
-		switch {
-		case r == 3: // 9
-			ok = true
-		case r < 3: // 6,7,8 require next rank
-			ok = (played & (uint64(1) << (i + 1))) != 0
-		default: // T,J,Q,K,A require previous rank
-			ok = (played & (uint64(1) << (i - 1))) != 0
-		}
-		if ok {
-			moves = append(moves, bit)
+func (g *gameSolver) hasMove(state int, rem uint64) bool {
+	for i := 0; i < int(stateMoveCount[state]); i++ {
+		if rem&stateMoves[state][i].bit != 0 {
+			return true
 		}
 	}
-	return moves
+	return false
 }
 
 // solve returns game value from Alice's perspective:
 // +x -> Alice wins, Bob has x cards left; -y -> Bob wins, Alice has y cards left.
-func (g *gameSolver) solve(played uint64, turn int) int {
-	key := (played << 1) | uint64(turn)
-	if v, ok := g.memo[key]; ok {
-		return v
+func (g *gameSolver) solve(state, turn int) int {
+	if g.memo[turn][state] != 1<<30 {
+		return g.memo[turn][state]
 	}
 
+	played := stateMasks[state]
 	aliceRem := g.aMask &^ played
 	bobRem := g.bMask &^ played
 	if aliceRem == 0 {
 		v := bits.OnesCount64(bobRem)
-		g.memo[key] = v
+		g.memo[turn][state] = v
 		return v
 	}
 	if bobRem == 0 {
 		v := -bits.OnesCount64(aliceRem)
-		g.memo[key] = v
+		g.memo[turn][state] = v
 		return v
 	}
 
@@ -80,48 +154,59 @@ func (g *gameSolver) solve(played uint64, turn int) int {
 	if turn == 1 {
 		rem = bobRem
 	}
-	moves := g.legalMoves(rem, played)
-	if len(moves) == 0 {
+	if !g.hasMove(state, rem) {
 		oppRem := bobRem
 		if turn == 1 {
 			oppRem = aliceRem
 		}
-		if len(g.legalMoves(oppRem, played)) == 0 {
-			g.memo[key] = 0
+		if !g.hasMove(state, oppRem) {
+			g.memo[turn][state] = 0
 			return 0
 		}
-		v := g.solve(played, 1-turn)
-		g.memo[key] = v
+		v := g.solve(state, 1-turn)
+		g.memo[turn][state] = v
 		return v
 	}
 
 	if turn == 0 {
 		best := -1 << 30
-		for _, mv := range moves {
-			val := g.solve(played|mv, 1)
+		for i := 0; i < int(stateMoveCount[state]); i++ {
+			mv := stateMoves[state][i]
+			if rem&mv.bit == 0 {
+				continue
+			}
+			val := g.solve(mv.next, 1)
 			if val > best {
 				best = val
 			}
 		}
-		g.memo[key] = best
+		g.memo[turn][state] = best
 		return best
 	}
 
 	best := 1 << 30
-	for _, mv := range moves {
-		val := g.solve(played|mv, 0)
+	for i := 0; i < int(stateMoveCount[state]); i++ {
+		mv := stateMoves[state][i]
+		if rem&mv.bit == 0 {
+			continue
+		}
+		val := g.solve(mv.next, 0)
 		if val < best {
 			best = val
 		}
 	}
-	g.memo[key] = best
+	g.memo[turn][state] = best
 	return best
 }
 
 func importance(aMask, bMask uint64) int {
-	s := gameSolver{aMask: aMask, bMask: bMask, memo: make(map[uint64]int)}
+	s := gameSolver{aMask: aMask, bMask: bMask}
+	for turn := 0; turn < 2; turn++ {
+		for i := 0; i < totalStates; i++ {
+			s.memo[turn][i] = 1 << 30
+		}
+	}
 	vAliceFirst := s.solve(0, 0)
-	s.memo = make(map[uint64]int)
 	vBobFirst := s.solve(0, 1)
 	if vAliceFirst >= vBobFirst {
 		return vAliceFirst - vBobFirst
