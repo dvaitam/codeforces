80d5f25f13716a92bdec2c22dee304de84dee59e
dvaitam <137186606+dvaitam@users.noreply.github.com>
2026-03-01 11:47:45 +0530

    Fix 1716C verifier reference implementation

--- 1000-1999/1700-1799/1710-1719/1716/verifierC.go
+++ 1000-1999/1700-1799/1710-1719/1716/verifierC.go
@@ -18,31 +18,45 @@ func max(a, b int) int {
 }
 
 func solveCase(m int, top, bottom []int) int {
-	prefTop := make([]int, m+1)
-	prefBottom := make([]int, m+1)
-	for i := 1; i <= m; i++ {
-		prefTop[i] = max(prefTop[i-1]+1, top[i]+1)
-		prefBottom[i] = max(prefBottom[i-1]+1, bottom[i]+1)
+	a := [2][]int{make([]int, m), make([]int, m)}
+	for i := 0; i < m; i++ {
+		a[0][i] = top[i+1]
+		a[1][i] = bottom[i+1]
 	}
-	sufTop := make([]int, m+2)
-	sufBottom := make([]int, m+2)
-	for i := m; i >= 1; i-- {
-		sufTop[i] = max(sufTop[i+1]+1, top[i]+1)
-		sufBottom[i] = max(sufBottom[i+1]+1, bottom[i]+1)
+
+	// Match the accepted solution exactly.
+	a[0][0] = -1
+	p := [2][]int{make([]int, m+1), make([]int, m+1)}
+	for i := m - 1; i >= 0; i-- {
+		for row := 0; row < 2; row++ {
+			p[row][i] = max(
+				max(a[1-row][i]+1, a[row][i]+(m-i)*2),
+				p[row][i+1]+1,
+			)
+		}
 	}
+
 	ans := int(^uint(0) >> 1)
-	for i := 1; i <= m; i++ {
-		cur1 := max(prefTop[i-1], sufBottom[i+1])
-		cur2 := max(prefBottom[i-1], sufTop[i+1])
-		cur := cur1
-		if cur2 < cur {
-			cur = cur2
-		}
-		if cur < ans {
-			ans = cur
-		}
+	n := 0
+	for i := 0; i < m; i++ {
+		c := i & 1
+		ans = min(ans, max(n, p[c][i]))
+		n = max(
+			n,
+			max(
+				a[c][i]+(m-i)*2,
+				a[1-c][i]+(m-i-1)*2+1,
+			),
+		)
 	}
 	return ans
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func runCandidate(bin, input string) (string, error) {
